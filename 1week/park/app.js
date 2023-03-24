83f69ccf53c7efd47137f5ef8134def9508ca7ee
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-24 11:37:28 +0900

    mySQL 모듈 사용

--- 1week/park/app.js
+++ 1week/park/app.js
@@ -77,22 +77,6 @@ const server = http.createServer(function(request, response) {
 })
 
 
-// const server = http.createServer(function(request, response){
-//   response.writeHead(200, {'Content-Type' : 'text/html'}); // 네트워크 header 값 설정
-//   response.write(mainPage); // 미리 선언해준 '문자열' 입력.
-//   if (request.method === 'POST') { // ! 요청 방식이 'POST'일 경우인 조건문 작성.
-//     let textData = '';
-//     request.on('data', function(data){ // request.on 메서드의 'data'가 실행될 때, 콜백함수 실행.
-//       textData = textData + data; // textData에 'data' 값을 담아줌.
-//     });
-//     request.on('end', function(){
-//       let parsedData = qs.parse(textData); // ! qs 모듈을 이용하여 값을 parsing함.
-//       console.log(parsedData.id); // ? 값이 잘 들어왔는지 확인.
-//       response.end(resultPage(parsedData.id)); // ! 값이 확인됐다면, 페이지에 출력.
-//     })
-//   }
-// });
-
 
 server.listen(2080, function() {
   console.log('server is runnig...');
