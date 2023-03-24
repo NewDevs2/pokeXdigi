bb532672906e0ff7cf8a751b0b1bc41148b3b753
KwakYounHo <kwakyh951@gmail.com>
2023-03-24 17:12:50 +0900

    메인 페이지

---
+++ 1week/kwak/JS/index.js
@@ -0,0 +1,10 @@
+// console.log('access')
+const LogInBurron = document.getElementById('logIn_B')
+const SignUpButton = document.getElementById('signUp_B');
+
+LogInBurron.addEventListener('click',()=>{
+  location.href = './HTML/logIn.html';
+})
+SignUpButton.addEventListener('click',()=>{
+  location.href = './HTML/signUp.html';
+})
