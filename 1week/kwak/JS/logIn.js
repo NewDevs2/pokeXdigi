945c0982986d3369cd2c984fc9b806d690a55c89
KwakYounHo <kwakyh951@gmail.com>
2023-03-24 17:13:18 +0900

    로그인 로직

---
+++ 1week/kwak/JS/logIn.js
@@ -0,0 +1,10 @@
+const HomeButton = document.getElementById('GoHome');
+const form       = document.getElementById('logInForm');
+
+// 홈으로
+HomeButton.addEventListener('click',()=>{
+  location.href  = '../index.html';
+})
+
+// console.log(HomeButton);
+// console.log(form);
