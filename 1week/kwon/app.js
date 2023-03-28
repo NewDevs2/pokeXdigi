3cbd53a9b16d1dabf29bd2eae6119617b1fc511c
gwonyejun <kyjuni1059@gmail.com>
2023-03-28 18:00:08 +0900

    디비 모듈화 연습 , 회원가입 유효성검사 추가 작업 완료 하고 디비 타입 설정 해야 한다.

--- 1week/kwon/app.js
+++ 1week/kwon/app.js
@@ -2,7 +2,9 @@ import fs from 'fs';
 import http from 'http';
 import qs from 'qs';
 import mysql from 'mysql2';
-const twoText = (id,pw) => {
+import idn from './idnone.js';
+
+const twoText = (id, pw) => {
   return `<!DOCTYPE html>
   <html lang="en">
   
@@ -33,6 +35,14 @@ const twoText = (id,pw) => {
 
 const server = http.createServer(function (request, response) {
   let a = true;
+  //텍스트 파일을 불러와 데이터를 뿌려주었다.
+  const testjoina = fs.readFileSync('./join.txt', { encoding: 'utf-8' });
+  //로그인 페이지 이동
+  const testlogin = fs.readFileSync('./login.txt', { encoding: 'utf-8' });
+  const mainpg = fs.readFileSync('./mainpg.txt', { encoding: 'utf-8' });
+  // ! 메인 페이지를 가져온다.
+  const errpg = fs.readFileSync('./err.txt', { encoding: 'utf-8' });
+  // ! 에러 페이지 가져오기
   const conn = {
     host: 'localhost',
     user: 'root',
@@ -45,7 +55,7 @@ const server = http.createServer(function (request, response) {
   // ! 메인 페이지
   if (request.method === 'GET' && request.url === '/') {
     //! 조건문을 사용 할 때 메소드를 받을 때 영어를 대문자로 해주어야 인식 한다. 소문자로 입력을 하게 되면 데이터를 못 받더라.
-    const mainpg = fs.readFileSync('./mainpg.txt', { encoding: 'utf-8' });
+
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text-html');
     response.end(mainpg);
@@ -53,8 +63,7 @@ const server = http.createServer(function (request, response) {
 
   // ! 회원가입 페이지
   if (request.method === 'GET' && request.url === '/join') {
-    //텍스트 파일을 불러와 데이터를 뿌려주었다.
-    const testjoina = fs.readFileSync('./join.txt', { encoding: 'utf-8' });
+
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text-html');
     response.end(testjoina);
@@ -62,8 +71,7 @@ const server = http.createServer(function (request, response) {
 
   //! 로그인 페이지
   if (request.method === 'GET' && request.url === '/login') {
-    //로그인 페이지 이동
-    const testlogin = fs.readFileSync('./login.txt', { encoding: 'utf-8' });
+
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text-html');
     response.end(testlogin);
@@ -72,6 +80,7 @@ const server = http.createServer(function (request, response) {
 
   // ! 회원가입 정보 db로 넘기기
   if (request.method === 'POST' && request.url === '/login') {
+
     let testdata = '';
     request.on('data', function (data) {
       testdata = testdata + data;
@@ -80,35 +89,27 @@ const server = http.createServer(function (request, response) {
     request.on('end', function () {
 
       let qsdata = qs.parse(testdata);
-    
+        
+        co.query(`insert into jt(id,jumin,pw) values ('${qsdata.id}','${qsdata.jumin}','${qsdata.pw}');`, function (err, results, fields) {
 
-      co.query(`insert into jt(id,jumin,pw) values ('${qsdata.id}','${qsdata.jumin}','${qsdata.pw}');`, function (err, results, fields) {
+          if (err) { console.log(err) };
+          console.log(results);
 
-        if (err) { console.log(err) };
-        console.log(results);
+        });
+        //! 값 저장 해둘려고 시도
+        co.query(`select * from jt`, function (err, results, fields) {
 
-      });
-   
-
-      //! 값 저장 해둘려고 시도
-      co.query(`select * from jt`, function (err, results, fields) {
-
-        if (err) {
-          console.log(err);
-        };
-     
-        fs.writeFileSync('./userinfo.js', JSON.stringify(results));
-      
-      });
-
-      const testlogin = fs.readFileSync('./login.txt', { encoding: 'utf-8' });
-   
-      response.end(testlogin);
-
-    })
+          if (err) {
+            console.log(err);
+          };
+          console.log(results);
+        });
+        response.end(testlogin);
+        // else if(qsdata.jumin === '' || qsdata.jumin.length > 14)
+        // {response.end(idn());}
+    });
   }
 
-  const errpg = fs.readFileSync('./err.txt', { encoding: 'utf-8' });
 
   // ! 로르인 하기
   if (request.method === 'POST' && request.url === '/userpg') {
@@ -119,32 +120,33 @@ const server = http.createServer(function (request, response) {
     })
     request.on('end', function () {
       let qsdata = qs.parse(testdata);
-      co.query(`select id,pw from jt where id = ${qsdata.id} and pw = ${qsdata.pw} `, function (err, results, fields) {
+      console.log(qsdata.id);
+      co.query(`select id,pw from jt where id = ${qsdata.id} and pw = ${qsdata.pw}`, function (err, results, fields) {
+        try {
+          if (err) {
+            console.log(err);
+          };
 
-        if (err) {
-          console.log(err);
-        };
-   
-
-        try{
-          if(results[0].id === qsdata.id && results[0].pw === qsdata.pw  ){
+          if (results[0].id === qsdata.id && results[0].pw === qsdata.pw) {
             console.log(qsdata.id);
             console.log(results[0].id);
-    
-            response.end(twoText(qsdata.id,qsdata.pw));  
+            response.end(twoText(qsdata.id, qsdata.pw));
             // ! 로그인 할려고 하는 정보와 일치 하면 페이지를 넘긴다.
-            }
+          }
         }
-        catch(err){
-       alert(`실패`);
+        catch (err) {
+          // ! 예외처리 test
+          console.log('열결 확인');
+          response.end(errpg);
+          // ! 정보와 일치 하지 않으면 에러 페이지 출력을 한다.
         }
-   
+
         // ? 로그인 할 때 회원정보 있는지 db에 확인을 하고 나서 페이지 넘김
-    
+
         // ! 로그인 성고 확인
       });
-    
-    
+
+
     })
   }
 });
