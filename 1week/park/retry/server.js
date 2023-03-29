c05ea21d09038f33a95cd72298f1cfd8c7abcd1d
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-29 17:17:03 +0900

    리빌드

---
+++ 1week/park/retry/server.js
@@ -0,0 +1,61 @@
+import http from 'http' 
+import mysql2 from 'mysql2';
+import fs from 'fs';
+// ? 페이지
+import { firstPage } from '../page.js';
+
+// ? 앱 서버
+
+const appServer = http.createServer(function(request, response) {
+  if (request.method === 'GET' && request.url === '/') {
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.write(firstPage);
+    response.end();
+  }
+  if (request.method === 'POST' && request.url.startsWith('/login')){
+    response.write(loginPage);
+    response.end();
+  }
+  if (request.method === 'POST' && request.url.startsWith('/create')) {
+    response.write(createAccountPage);
+    response.end();
+  }
+})
+
+
+appServer.listen(2080, function () {
+  console.log('Appserver is Running. . .');
+})
+
+// post 요청이 왔을 때, 그 자체가 clickevent다
+// json을 만들어줘 등등 시도해볼것
+
+// ? DB 서버
+
+
+const conn = mysql2.createConnection({
+  host       : 'localhost',
+  user       : 'root',
+  password   : 'VHzmffkr1208',
+  database   : 'user_info',
+  port       :  3306,
+  socketPath : '/tmp/mysql.sock'
+});
+
+conn.connect();
+let conncetionTest = `show databases;`
+console.log('DB server is Running. . .');
+
+// conn.query(conncetionTest, function(err, result, fields) {
+//   if (err) throw err;
+//   console.log(result);
+// })
+
+// const dbServer = http.createServer(function(request, response) {
+//   if (request.method === 'GET' && request.url === '/') {
+//   }
+// })
+
+// conn.listen(3080, function () {
+//   console.log('DBserver is Running');
+// })
