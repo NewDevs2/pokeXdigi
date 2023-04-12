82a59d9c12408d39f932c79e06c43834f602e448
krapli441 <tlfqj1208@gmail.com>
2023-04-12 21:08:55 +0900

    리팩토링 중간저장. 개인정보 문구 넣어야 함

--- JS/createAccount.js
+++ JS/createAccount.js
@@ -3,7 +3,7 @@
 const wrap = document.getElementById("wrap");
 const container = document.getElementById("container");
 
-// ! 태그 생성하는 함수 제작
+// ! 태그 생성 함수
 const createTag = function (tagName, attribute, className, where, text) {
   const element = document.createElement(tagName);
   if (element) {
@@ -26,17 +26,31 @@ createTag("div", "class", "account_information", container);
 createTag("form", "class", "createAccountForm", container.children[1]);
 const form = document.querySelector(".createAccountForm");
 
-// ! input 태그 생성
+// ! input 태그 생성 함수
 const createInput = function (type, name, id, placeholder, required, where) {
   const input = document.createElement("input");
   input.type = type;
   input.name = name;
   input.id = id;
-  input.placeholder = placeholder;
-  input.required = required;
+  if (placeholder !== "") {
+    input.placeholder = placeholder;
+  }
+  if (required !== "") {
+    input.required = required;
+  }
 
   where.appendChild(input);
   return input;
+};
+
+// ! label 태그 생성 함수
+const createLabel = function (name, where, text) {
+  const label = document.createElement("label");
+  label.setAttribute("for", name);
+  if (text) {
+    label.innerHTML = text;
+  }
+  where.appendChild(label);
 };
 
 createInput("text", "id", "id", "아이디", true, form);
@@ -63,5 +77,18 @@ createInput(
 
 // ! 개인정보 & 마케팅 동의 체크박스 생성 및 식별
 createTag("div", "class", "checkbox", form);
-const checkbox = document.querySelector(".checbox");
-console.log(checkbox);
+const checkbox = document.querySelector(".checkbox");
+
+createTag("div", "class", "personal_header", checkbox, "개인정보수집활용");
+createTag("div", "class", "personal_checker", checkbox);
+createInput("radio", "person_info_agreed", "", "", true, checkbox.children[1]);
+createLabel("personal_check_agreed", checkbox.children[1], "동의함");
+createInput(
+  "radio",
+  "person_info_disagreed",
+  "",
+  "",
+  true,
+  checkbox.children[1]
+);
+createLabel("personal_check_disagreed", checkbox.children[1], "동의하지 않음");
