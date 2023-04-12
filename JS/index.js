306c91bb7b2e93263d5d807d58885353ddec0a0d
gwonyejun <kyjuni1059@gmail.com>
2023-04-12 14:47:50 +0900

    메인 회원찾기 js

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+function blockMaker(tagName, attribute, idName, path) {
+  const blockName = document.createElement(tagName);
+  const pathName = document.querySelector(path);
+  blockName.setAttribute(attribute, idName);
+  pathName.appendChild(blockName);
+  console.dir(blockName);
+}
+function textMaker(path, child, area) {
+  const pathName = document.querySelector(path);
+  const textTag = document.createElement(child);
+  pathName.appendChild(textTag);
+  textTag.innerText = area;
+  console.log();
+}
+
+blockMaker("div", "id", "root", "body"); // * 바디 자식으로 루트 id를 가진 디비전 만듦
+blockMaker("a", "href", "", "body > #root");
+blockMaker("a", "href", "", "body > #root");
+// * #root안에 a태그를 만들어주었다.
+
+textMaker("#root > :nth-child(1)", "p", "sign in");
+textMaker("#root > :nth-child(2)", "p", "sign up");
+// * a태그 자식 밑에 텍스트를 넣어주었다.
