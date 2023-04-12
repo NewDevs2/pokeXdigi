6bdf613ff1859d3e08b97f44841a56629cb66f31
krapli441 <tlfqj1208@gmail.com>
2023-04-12 20:08:40 +0900

    리팩토링 작업 중. 우선 예비 파일 생성

--- JS/createAccount.js
+++ JS/createAccount.js
@@ -1,1 +1,21 @@
-// 리팩토링 해봅시다
+// ! wrap, container 식별
+
+const wrap = document.getElementById("wrap");
+const container = document.getElementById("container");
+
+// ! 태그 생성하는 함수 제작
+const createTag = function (tagName, attribute, className, where, text) {
+  const element = document.createElement(tagName);
+  if (element) {
+    element.setAttribute(attribute, className);
+  }
+  if (where) {
+    where.appendChild(element);
+  }
+  if (text) {
+    element.innerHTML = text;
+  }
+  return element;
+};
+
+createTag("h1", "class", "header", container, "계정 생성");
