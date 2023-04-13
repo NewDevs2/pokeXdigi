c2e3bf5717c0e54330fd2f2d791a3291505d5ae8
gwonyejun <kyjuni1059@gmail.com>
2023-04-13 14:32:36 +0900

    메인 페이지와 회원찾기 페이지를 js 파일도 함께 열릴수 있게 작업을 하였다.

--- JS/controller.js
+++ JS/controller.js
@@ -1,18 +1,36 @@
 import http from "http";
 import fs from "fs";
 
-export default http.createServer((req, rep) => {
+const server = http.createServer((req, rep) => {
   try {
     if (req.method === "GET") {
       if (req.url === "/") {
-        const page = fs.readFileSync("./HTML/index.html",'UTF-8')
-        rep.writeHead(200, {'Content-Type':'text/html; charset=UTF-8;'});
+        const page = fs.readFileSync("../HTML/index.html", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
         rep.write(page);
         rep.end();
       }
-    } 
+       else if (req.url === "/JS/index.js") { 
+        const script = fs.readFileSync("./index.js", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/javascript; charset=UTF-8;" });
+        rep.write(script);
+        rep.end();
+      }
+      else if (req.url === "/findAccount.html") { 
+        const page = fs.readFileSync("../HTML/findAccount.html", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
+        rep.write(page);
+        rep.end();
+      }
+      else if (req.url === "/JS/findAccount.js") { 
+        const script = fs.readFileSync("./findAccount.js", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/javascript; charset=UTF-8;" });
+        rep.write(script);
+        rep.end();
+      }
+    }
   } catch {
-
+    // 예외 처리
   }
 });
 server.listen(8080,(err)=> {
