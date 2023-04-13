eb6625b624c8b63d6e58ac19b174a1f152d58182
HaekyungSeong <haekyungs95@gmail.com>
2023-04-13 15:05:46 +0900

    충돌방지를 위해 if문 별도 txt 파일로 작성 후 재커밋

--- JS/controller.js
+++ JS/controller.js
@@ -4,19 +4,13 @@ import fs from "fs";
 export default http.createServer((req, rep) => {
   try {
     if (req.method === "GET") {
-      if (req.url === "./HTML/loginFail.html") {
-        const page = fs.readFileSync("../HTML/loginFail.html",'UTF-8')
+      if (req.url === "/") {
+        const page = fs.readFileSync("./HTML/index.html",'UTF-8')
         rep.writeHead(200, {'Content-Type':'text/html; charset=UTF-8;'});
         rep.write(page);
         rep.end();
       }
-      else if (req.url === "./JS/loginFail.js") {
-        const page = fs.readFileSync("../JS/loginFail.js",'UTF-8')
-        rep.writeHead(200, {'Content-Type':'text/javascript; charset=UTF-8;'});
-        rep.write(page);
-        rep.end();
-      }
-    }
+    } 
   } catch {
 
   }
