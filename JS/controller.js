d0473d672cad87797c92efdbc1aabbef574e0bde
gwonyejun <kyjuni1059@gmail.com>
2023-04-13 15:33:55 +0900

    이슈#6에서 만든 페이지 서버 로직에 응답 작성&주석

--- JS/controller.js
+++ JS/controller.js
@@ -4,28 +4,91 @@ import fs from "fs";
 const server = http.createServer((req, rep) => {
   try {
     if (req.method === "GET") {
+      //* 최초 접속
       if (req.url === "/") {
         const page = fs.readFileSync("../HTML/index.html", "UTF-8");
         rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
         rep.write(page);
         rep.end();
       }
-       else if (req.url === "/JS/index.js") { 
+      //* 메인 페이지 js파일
+      if (req.url === "/JS/index.js") {
         const script = fs.readFileSync("./index.js", "UTF-8");
-        rep.writeHead(200, { "Content-Type": "text/javascript; charset=UTF-8;" });
+        rep.writeHead(200, {
+          "Content-Type": "text/javascript; charset=UTF-8;",
+        });
         rep.write(script);
         rep.end();
       }
-      else if (req.url === "/findAccount.html") { 
+      //* 계정 찾기 페이지
+      if (req.url === "/findAccount.html") {
         const page = fs.readFileSync("../HTML/findAccount.html", "UTF-8");
         rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
         rep.write(page);
         rep.end();
       }
-      else if (req.url === "/JS/findAccount.js") { 
+      //* 계정 찾기 js파일
+      if (req.url === "/JS/findAccount.js") {
         const script = fs.readFileSync("./findAccount.js", "UTF-8");
-        rep.writeHead(200, { "Content-Type": "text/javascript; charset=UTF-8;" });
+        rep.writeHead(200, {
+          "Content-Type": "text/javascript; charset=UTF-8;",
+        });
         rep.write(script);
+        rep.end();
+      }
+      //* 로그인 페이지
+      if (req.url.includes("login.html")) {
+        const data = fs.readFileSync("../HTML/login.html", "utf-8");
+        rep.writeHead(200, { "Content-Tpye": "text/html; charset=utf-8" });
+        rep.write(data);
+        rep.end();
+      }
+      //* 로그인 페이지 css
+      if (req.url.includes("login.css")) {
+        const data = fs.readFileSync("../CSS/login.css", "utf-8");
+        rep.writeHead(200, { "Content-Tpye": "text/css; charset=utf-8" });
+        rep.write(data);
+        rep.end();
+      }
+      //* 로그인 페이지 js파일
+      if (req.url.includes("login.js")) {
+        const data = fs.readFileSync("./login.js", "utf-8");
+        rep.writeHead(200, {
+          "Content-Tpye": "text/javascript; charset=utf-8",
+        });
+        rep.write(data);
+        rep.end();
+      }
+      //* 회원가입 성공 페이지 - 루빈
+      if (req.url === "/accountSuccess.html") {
+        const page = fs.readFileSync("../HTML/accountSuccess.html", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
+        rep.write(page);
+        rep.end();
+      }
+      //* 회원가입 성공 페이지 js파일
+      if (req.url == "/JS/accountSuccess.js") {
+        const jsPage = fs.readFileSync("./accountSuccess.js", "UTF-8");
+        rep.writeHead(200, {
+          "Content-Type": "text/javascript; charset=UTF-8;",
+        });
+        rep.write(jsPage);
+        rep.end();
+      }
+      //* 로그인 실패 페이지
+      if (req.url === "/HTML/loginFail.html") {
+        const page = fs.readFileSync("../HTML/loginFail.html", "UTF-8");
+        rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
+        rep.write(page);
+        rep.end();
+      }
+      //* 로그인 실패 js파일
+      if (req.url === "/JS/loginFail.js") {
+        const page = fs.readFileSync("./loginFail.js", "UTF-8");
+        rep.writeHead(200, {
+          "Content-Type": "text/javascript; charset=UTF-8;",
+        });
+        rep.write(page);
         rep.end();
       }
     }
@@ -33,7 +96,10 @@ const server = http.createServer((req, rep) => {
     // 예외 처리
   }
 });
-server.listen(8080,(err)=> {
-  if(err) {console.log(err);throw err;}
-  console.log("서버 접속 성공")
-})
+server.listen(8080, (err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("서버 접속 성공");
+});
