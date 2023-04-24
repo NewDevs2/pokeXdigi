38fc3afe981498068bb7a744643f54e174070e5b
gwonyejun <kyjuni1059@gmail.com>
2023-04-24 17:51:50 +0900

    리팩토링 하면서 남겨둔 주석 처리 하기 Fixes #20

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,43 +1,19 @@
-// function blockMaker(tagName, attribute, idName, path) {
-//   const blockName = document.createElement(tagName);
-//   const pathName = document.querySelector(path);
-//   blockName.setAttribute(attribute, idName);
-//   pathName.appendChild(blockName);
-//   console.dir(blockName);
-// }
-// function textMaker(path, child, area) {
-//   const pathName = document.querySelector(path);
-//   const textTag = document.createElement(child);
-//   pathName.appendChild(textTag);
-//   textTag.innerText = area;
-//   console.log();
-// }
-
-// blockMaker("div", "id", "root", "body"); // * 바디 자식으로 루트 id를 가진 디비전 만듦
-// blockMaker("a", "href", "/src/views/html/login.html", "body > #root");
-// blockMaker("a", "href", "/src/views/html/createAccount.html", "body > #root");
-// // * #root안에 a태그를 만들어주었다.
-
-// textMaker("#root > :nth-child(1)", "p", "sign in");
-// textMaker("#root > :nth-child(2)", "p", "sign up");
-// // * a태그 자식 밑에 텍스트를 넣어주었다.
 
 import tagMaker from "../../models/tag/tagMaker.js";
 
+// ! body 태그와 같은 크기를 준다.
+const root = tagMaker("div", document.body, {
+  id: "root",
+  style:
+    " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
+});
 
-   // ! body 태그와 같은 크기를 준다.
-    const root = tagMaker('div', document.body, {
-    id: 'root',
-    style:' width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;'
-  });
-
-    //! 버튼 만들기
-    const btnback = tagMaker('button',root,{innerText:'sing-in'});
-    btnback.addEventListener('click',function(){
-      location.href = '/src/views/html/login.html';
-    });
-    const btnback2 = tagMaker('button',root,{innerText:'sing-up'});
-    btnback2.addEventListener('click',function(){
-      location.href = '/src/views/html/createAccount.html';
-    });
-  
+//! 버튼 만들기
+const btnback = tagMaker("button", root, { innerText: "sing-in" });
+btnback.addEventListener("click", function () {
+  location.href = "/src/views/html/login.html";
+});
+const btnback2 = tagMaker("button", root, { innerText: "sing-up" });
+btnback2.addEventListener("click", function () {
+  location.href = "/src/views/html/createAccount.html";
+});
