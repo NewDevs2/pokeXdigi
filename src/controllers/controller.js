f09355c3a674e12bd230ec1f228b16be14ee5708
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 17:17:34 +0900

    오타 수정

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -4,7 +4,7 @@ import qs from "querystring";
 import path from "path";
 import { fileURLToPath } from "url";
 import sign_master from "../models/DBConfig.js";
-import responseMdoule from "../../issue/21/responseModule.js"
+import responseModule from "../../issue/21/responseModule.js"
 
 
 const __fileName = fileURLToPath(import.meta.url);
