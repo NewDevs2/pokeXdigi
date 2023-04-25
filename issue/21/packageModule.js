2992aaafc3749b335d2edac123e378da88838953
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 16:03:58 +0900

    패키지 모듈 내보내기 테스트

---
+++ issue/21/packageModule.js
@@ -0,0 +1,11 @@
+import http from "http"
+import mysql from "mysql2"
+import fs from "fs"
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const http = http
+export const mysql = mysql
+export const fs = fs
+export const path = path
+export const fileURLToPath = fileURLToPath
