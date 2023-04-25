8e112b5e2277744dc74134d9e92e4544c3f89811
jangrubin <wkdfnqls2465@gmail.com>
2023-04-25 16:43:50 +0900

    오타 수정

--- issue/21/responseModule/accountSuccessModule.js
+++ issue/21/responseModule/accountSuccessModule.js
@@ -1,7 +1,7 @@
 
 if (req.url.includes("html/accountSuccess.html")) {
-  responseMdoule(200, "text/html", req, rep);
+  responseModule(200, "text/html", req, rep);
 }
 if (req.url.includes("js/accountSuccess.js")) {
-  responseMdoule(200, "text/javascript", req, rep);
+  responseModule(200, "text/javascript", req, rep);
 }
