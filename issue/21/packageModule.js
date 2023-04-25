6675bcad77924fcca7ae1541d833857cb2f2c70e
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 16:23:24 +0900

    객체형태로 packageModule export 테스트(성공)

--- issue/21/packageModule.js
+++ issue/21/packageModule.js
@@ -1,13 +1,8 @@
-import http1 from "http"
-import mysql1 from "mysql2"
-import fs1 from "fs"
-import path1 from "path";
+import http from "http"
+import mysql from "mysql2"
+import fs from "fs"
+import path from "path";
 import { fileURLToPath } from "url";
-export const test = {
-  http : http1,
-  mysql : mysql1,
-  fs : fs1,
-  path : path1
 
+export default {http, mysql, fs, path, fileURLToPath} 
 // export const fileURLToPath = fileURLToPath
-}
