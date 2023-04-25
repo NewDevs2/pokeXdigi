2ae6dcbce9a1e7bdf0992d1fe9053294113cf8e4
gwonyejun <kyjuni1059@gmail.com>
2023-04-25 12:13:23 +0900

    post 미완성

--- issue/#21/kwonbook.js
+++ issue/#21/kwonbook.js
@@ -3,18 +3,18 @@ import fs from "fs";
 import qs from "querystring";
 import path from "path";
 import { fileURLToPath } from "url";
-// import sign_master from "../models/DBConfig.js";
+import sign_master from "../models/DBConfig.js";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 const root = path.join(__dirName, "../../");
 
-// sign_master.connect(function (err) {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("DB 연결");
-// });
+sign_master.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("DB 연결");
+});
 
 function reqMethodGet(reqUrl, rep) {
   //* 최초 접속
@@ -26,13 +26,11 @@ function reqMethodGet(reqUrl, rep) {
     rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
     rep.write(page);
     rep.end();
-  } 
-  else if(reqUrl.includes('.html')){
+  } else if (reqUrl.includes(".html")) {
     rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
     rep.write(fs.readFileSync(path.join(root, reqUrl), "utf-8"));
     rep.end();
-  }
- else if (reqUrl.includes(".css")) {
+  } else if (reqUrl.includes(".css")) {
     rep.writeHead(200, { "Content-Type": "text/css; charset=UTF-8;" });
     rep.write(fs.readFileSync(path.join(root, reqUrl), "utf-8"));
     rep.end();
@@ -43,6 +41,14 @@ function reqMethodGet(reqUrl, rep) {
   }
 }
 
+ function postData(req) {
+  let userData = "";
+  req.on("data", (chunk) => {
+    userData += chunk;
+  });
+  return userData;
+}
+
 const server = http.createServer((req, rep) => {
   try {
     switch (req.method) {
@@ -51,6 +57,116 @@ const server = http.createServer((req, rep) => {
         break;
 
       case "POST":
+        if (req.url.includes("/html/checkCreateAccount")) {
+          let data = "";
+          req.on("data", (chunk) => {
+            data += chunk;
+          });
+
+          req.on("end", () => {
+            const userData = qs.parse(data);
+            // console.log(userData)
+            fs.writeFileSync(
+              path.join(root, "temp", `${userData.id}_createAccountCheck.JSON`),
+              JSON.stringify(userData)
+            );
+            // !변수 이름 바꿔줘 제발
+            // 제이슨 파일 가져와서 파싱하는 구간
+            const createAccountCheck = fs.readFileSync(
+              path.join(root, "temp", `${userData.id}_createAccountCheck.JSON`),
+              "utf-8"
+            );
+            const parsedCreateAccountCheck = JSON.parse(createAccountCheck);
+            // console.log(parsedCreateAccountCheck)
+            const column = Object.keys(parsedCreateAccountCheck).join();
+            const values = Object.values(parsedCreateAccountCheck)
+              .map((element) => {
+                return "'" + element + "'";
+              })
+              .join();
+            console.log(column, values);
+            sign_master.query(
+              `INSERT INTO user_information(${column}) values (${values})`,
+              (err, result) => {
+                fs.unlinkSync(
+                  path.join(
+                    root,
+                    "temp",
+                    `${userData.id}_createAccountCheck.JSON`
+                  )
+                );
+                if (err) {
+                  throw err;
+                }
+                rep.writeHead(200, { "Content-Type": "text/html" });
+                rep.write(
+                  `<script>location.href = "/src/views/html/accountSuccess.html"</script>`
+                );
+                rep.end();
+              }
+            );
+          });
+        }
+        // * 로그인 요청 들어왔을 때
+        if (req.url.includes("/checkLogin")) {
+          console.log("로그인 시도 테스트");
+          let userData = postData(req);
+          req.on("end", () => {
+            //* 클라이언트 인풋 데이터
+            let parsedData = qs.parse(userData);
+            // console.log(parsedData);
+            //* 회원 정보를 JSON 형태로 변환
+            fs.writeFileSync(
+              path.join(root, "temp", `${parsedData.UserID}_loginCheck.JSON`),
+              JSON.stringify(parsedData)
+            );
+            //* 클라이언트 인풋 JSON 데이터 파싱
+            const jsonCheck = fs.readFileSync(
+              path.join(root, "temp", `${parsedData.UserID}_loginCheck.JSON`),
+              "utf-8"
+            );
+            const parsedJsonCheck = JSON.parse(jsonCheck);
+            // console.log(parsedJsonCheck);
+            sign_master.query(
+              `SELECT ID,PASSWORD FROM user_information WHERE ID="${parsedJsonCheck.UserID}" AND PASSWORD="${parsedJsonCheck.UserPW}"`,
+              function (err, result, fields) {
+                if (err) {
+                  throw err;
+                }
+                console.log(result);
+                //* 대조 후 JSON 파일 삭제
+                fs.unlinkSync(
+                  path.join(
+                    root,
+                    "temp",
+                    `${parsedData.UserID}_loginCheck.JSON`
+                  )
+                );
+                //* 로그인 성공 / 실패 결과
+                if (result.length === 0) {
+                  //* 로그인 성공 시
+                  console.log("실패");
+                  rep.writeHead(200, { "Content-Type": "text/html" });
+                  rep.write(
+                    `<script>location.href = "/src/views/html/loginFail.html"</script>`
+                  );
+                } else if (result.length === 1) {
+                  //* 로그인 성공 시 메인 페이지로 이동
+                  console.log("성공");
+                  rep.writeHead(200, { "Content-Type": "text/html" });
+                  rep.write(
+                    `<script>location.href = "/src/views/html/index.html"</script>`
+                  );
+                  rep.end();
+                } else {
+                  console.log("뭔가 잘못됨");
+                  console.log(parsedData);
+                }
+              }
+            );
+          });
+        }
+
         break;
     }
   } catch (err) {
