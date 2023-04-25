23d4786e44fe65cd83e52924e46a10ed2a122b7b
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 15:22:59 +0900

    path.join의 절대경로로 사용하려 했던 경로가 req.url과 같은 것을 발견

--- issue/#21/responseModule.js
+++ issue/#21/responseModule.js
@@ -1,22 +1,25 @@
 // 서버 요청 응답 함수
-export default function(rep=rep) {
+export default function(rep=rep,req=req) {
+  // ! Default Parameter는 뒤에다가 놓으면 작성하지 않아도 실행 됨
   // req.url에 따라 content-type(mime)을 자동으로 정해주는 거 어때?
   // response의 writehead, write, end먼저 해보자
   // ?모듈로 사용할 때는 rep에 대한 정보를 매개변수로 넣어줘야 함
-  // ! mime만 바뀌는데 매번 작성해야돼? -> 
-  // ! if문의 조건(req.url)이 불편했다. ->
-  // ! fs.readFileSync 쓸 때 경로 쓰는 거 너무 불편해 ->
-  // ! 중복코드가 너무 많다. ->
+  // ! mime만 바뀌는데 매번 작성해야돼? -> 매개변수
+  // ! if문의 조건(req.url)이 불편했다. -> 매개변수
+  // ! fs.readFileSync 쓸 때 경로 쓰는 거 너무 불편해 -> 매개변수
+  // ! 중복코드가 너무 많다. -> 함수로 만들어
   // ? 바뀌는 부분은 리터럴로 사용, 바뀌는 부분만 default parameter로 사용하는 함수를 만들면 해결 할 수 있다.
   if (req.url.includes("js/index.js")) {
-    const script = fs.readFileSync(
-      path.join(root, "src", "views", "js", "index.js"),
+    // write에 담을 내용은 변수로 담을지 wite에서 처리할지 미정 알아서 해
+    const fileContent = fs.readFileSync(
+      // 뒤의 경로(파일 위치)이 그대로 담겨 옴
+      path.join(root, req.url),
       "UTF-8"
     );
     rep.writeHead(200, {
       "Content-Type": "text/javascript; charset=UTF-8;",
     });
-    rep.write(script);
+    rep.write(fileContent);
     rep.end();
   }
 }
