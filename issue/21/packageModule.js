4d8febe38b2c1f104943c5852ab95a4e91879e9e
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 16:16:59 +0900

    패키지 모듈 하나의 객체로 보내기 테스트 실패

--- issue/21/packageModule.js
+++ issue/21/packageModule.js
@@ -3,9 +3,11 @@ import mysql1 from "mysql2"
 import fs1 from "fs"
 import path1 from "path";
 import { fileURLToPath } from "url";
+export const test = {
+  http : http1,
+  mysql : mysql1,
+  fs : fs1,
+  path : path1
 
-export const http = http1
-export const mysql = mysql1
-export const fs = fs1
-export const path = path1
 // export const fileURLToPath = fileURLToPath
+}
