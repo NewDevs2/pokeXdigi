c4a12703c00c3413f79229b0004f488ea393ddae
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 16:12:15 +0900

    패키지 모듈 export test

--- issue/21/packageModule.js
+++ issue/21/packageModule.js
@@ -1,11 +1,11 @@
-import http from "http"
-import mysql from "mysql2"
-import fs from "fs"
-import path from "path";
+import http1 from "http"
+import mysql1 from "mysql2"
+import fs1 from "fs"
+import path1 from "path";
 import { fileURLToPath } from "url";
 
-export const http = http
-export const mysql = mysql
-export const fs = fs
-export const path = path
-export const fileURLToPath = fileURLToPath
+export const http = http1
+export const mysql = mysql1
+export const fs = fs1
+export const path = path1
+// export const fileURLToPath = fileURLToPath
