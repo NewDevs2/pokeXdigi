54ae116232829dab790b83c8b54ecc0e10b91dc7
HaekyungSeong <haekyungs95@gmail.com>
2023-04-25 12:01:26 +0900

    경로 별도 변수 없이 사용 확인

--- issue/#21/seong_test_controller.js
+++ issue/#21/seong_test_controller.js
@@ -1,6 +1,8 @@
+
 if (req.url === "/" || req.url.includes("index.html")) {
-
-  const filepath = path.join(root, "src", "views", "html", "index.html")
-  repPageWrite(filepath,rep,"html")
-
+  repPageWrite(path.join(root, "src", "views", "html", "index.html"), rep, "html")
+}
+//* 메인 페이지 js파일
+if (req.url.includes("js/index.js")) {
+  repPageWrite(path.join(root, "src", "views", "js", "index.js"), rep, "javascript")
 }
