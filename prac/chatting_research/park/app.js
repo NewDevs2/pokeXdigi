dde32437b1925a67bb5207206809e0721112239a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-26 15:17:14 +0900

    기초 파일 생성. connect 실험

---
+++ prac/chatting_research/park/app.js
@@ -0,0 +1,21 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer(function (req, rep) {
+  if (req.method === "GET") {
+    const page = fs.readFileSync("./index.html");
+    rep.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
+    rep.write(page);
+    rep.end();
+  }
+  if (req.method === "CONNCET") {
+    console.log("클라이언트가 요청했다.");
+  }
+});
+
+server.listen(5050, function (error) {
+  if (error) {
+    throw error;
+  }
+  console.log("server is running at localhost:5050");
+});
