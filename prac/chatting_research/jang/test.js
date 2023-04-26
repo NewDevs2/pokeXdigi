6c2f608dca65406d0283300b775ed699211daa2f
jangrubin <wkdfnqls2465@gmail.com>
2023-04-26 15:58:59 +0900

    내부에서는 사용 가능한 듯

--- prac/chatting_research/jang/test.js
+++ prac/chatting_research/jang/test.js
@@ -3,10 +3,13 @@ import WebSocket from "ws";
 
 const server = http.createServer((req, res) => {
   // 안에서 직접 포트 번호를 지정해주면 동작은 됨
-  const wss = new Server({port:8080})
-  wss.on("connection", (ws) => {
+  // 포트로 지정해줘도 되고 내부에서는 server도 담김
+  const wss = new Server({server})
+  wss.on("connection", () => {
   console.log("클라이언트가 연결 됐습니다");
-  })
+  
+})
+  
   res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
   res.end();
 });
