30c9f9af86c6d13a69856aa34608f90caf6b9b19
jangrubin <wkdfnqls2465@gmail.com>
2023-04-27 11:45:43 +0900

    소켓 1차시도

--- prac/chatting_research/jang/test3.js
+++ prac/chatting_research/jang/test3.js
@@ -1,8 +1,28 @@
 import http from "http"
+import {Server} from "socket.io"
 
-http.createServer((req,rep) => {
+const server = http.createServer((req,rep) => {
   rep.writeHead(200,{'Content-Type' : 'text/html;'})
   rep.end()
-}).listen(8080,()=> {
+});
+// Server 클래스를 가져와서 안에 있는 메서드로 무언가를 하는 듯
+const io = new Server(server);
+console.log(io)
+
+io.on("connection", (socket) => {
+  console.log("유저가 연결 됐습니다");
+
+  socket.on("disconnect", () => {
+    console.log("유저가 연결에 실패했습니다");
+  });
+
+  socket.on("chatMessage", (msg) => {
+    console.log("message:" + msg);
+    io.emit("chat message",msg);
+  });
+});
+
+
+server.listen(8080,()=> {
   console.log("정상 연결 됨")
 })
