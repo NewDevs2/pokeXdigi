84797b6f1b3f368c53fba2847e8d53f66898ba54
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-27 12:21:32 +0900

    socket.io 오류

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -1,24 +1,20 @@
-import http from "http";
+import { createServer } from "http";
 import fs from "fs";
+import { server } from "socket.io";
 
-const server = http.createServer(function (req, rep) {
+const httpServer = createServer((req, rep) => {
   if (req.url === "/") {
     const page = fs.readFileSync("./index.html");
-    rep.writeHead(200, { "Content-Type": "text/html; UTF-8;" });
+    rep.writeHead(200, { "Content-Type": "text/html; UTF-8" });
     rep.write(page);
-    return rep.end();
-  }
-
-  if (req.url === "/chat.js") {
-    const page = fs.readFileSync("./chat.js");
-    rep.writeHead(200, { "Content-Type": "text/javascript; UTF-8;" });
-    rep.write(page);
-    return rep.end();
+    rep.end();
   }
 });
 
-server.listen(5050, function (error) {
-  console.log("server is on");
+const io = new Server(httpServer);
+
+httpServer.listen(5050, function (error) {
+  console.log("server is runnig");
   if (error) {
     throw error;
   }
