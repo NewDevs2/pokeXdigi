61b409508bc7a066d32707c5d7523efcf9db3f36
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-27 11:52:17 +0900

    오류 해결 전 커밋

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -4,14 +4,14 @@ import fs from "fs";
 const server = http.createServer(function (req, rep) {
   if (req.method === "GET") {
     const page = fs.readFileSync("./index.html");
-    rep.writeHead(200, { "Content-Type": "text/html; UTF-8" });
+    rep.writeHead(200, { "Content-Type": "text/html; UTF-8;" });
     rep.write(page);
     rep.end();
   }
 
   if (req.url === "./chat.js") {
     const page = fs.readFileSync("./chat.js");
-    rep.writeHead(200, { "Content-Type": "text/javascript; UTF-8" });
+    rep.writeHead(200, { "Content-Type": "text/javascript; UTF-8;" });
     rep.write(page);
     rep.end();
   }
