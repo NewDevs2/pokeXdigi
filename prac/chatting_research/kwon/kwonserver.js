5b99d26cb35f6aec2a0d312b74243dc4cf314900
gwonyejun <kyjuni1059@gmail.com>
2023-04-27 13:12:16 +0900

    공백값 낫

--- prac/chatting_research/kwon/kwonserver.js
+++ prac/chatting_research/kwon/kwonserver.js
@@ -1,25 +1,45 @@
 import http from "http";
-import { Server } from "socket.io";
 import fs from "fs";
+import { Server } from "socket.io";
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(fs.readFileSync("./kwonclient.html", "utf-8"));
 });
-
 const io = new Server(server);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
+    // try{
+    //   if(msg !== ""){
+    //     console.log("message: " + msg);
+    //     io.emit("chat message", msg);
+    //   }
+    // }
+    // catch(error){
+
+    // }
+
+    try {
+      if (msg !== "") {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+      }
+      else{
+        alert('공백을 입력 하셨습니다.');
+      }
+    } catch (error) {
+      
+    }
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
+
 server.listen(2222, (err) => {
   if (err) {
     return console.log("something bad happened", err);
