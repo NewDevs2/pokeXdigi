342cd1d1ee1094604d01658ea75dcf9c16dc1a87
gwonyejun <kyjuni1059@gmail.com>
2023-04-27 19:22:49 +0900

    id값 메세지 작성

--- prac/chatting_research/kwon/kwonserver.js
+++ prac/chatting_research/kwon/kwonserver.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
+  const apt = '권';
   res.end(fs.readFileSync("./kwonclient.html", "utf-8"));
 });
 const io = new Server(server);
@@ -24,10 +25,19 @@ io.on("connection", (socket) => {
 
     try {
       if (msg !== "") {
-        const asdf = JSON.stringify(msg);
-        JSON.parse(asdf);
-        console.log( asdf.id );
-        io.emit("chat message", asdf);
+        // ! JSON으로 넘어온 값을 객체로 파싱 해준다.
+        const msgParse = JSON.parse(msg);
+        console.log(socket.apt);
+        // ! 객체로 된 값을 객체 넣어 한번데 클라이언트로 보내어 준다.
+        const objtest = {
+          id: msgParse.id,
+          message: msgParse.message
+        };
+        //  ! 클라이언트에 보내주기 전에 다시 JSON으로 바꾸어 준다.
+        const js = JSON.stringify(objtest);
+        // console.log(msg)
+        console.log(  objtest );
+        io.emit("chat message",js);
       } else {
         alert("공백을 입력 하셨습니다.");
       }
