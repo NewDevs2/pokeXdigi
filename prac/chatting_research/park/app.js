b5af3cf338b442246e7ff266c88e2d3ed9d0dff6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-27 15:53:19 +0900

    socket.io 사용하지 않고 채팅 구현해보기.

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -1,21 +1,19 @@
-import { createServer } from "http";
+import http from "http";
 import fs from "fs";
-import { Server } from "socket.io";
+import { error } from "console";
 
-const httpServer = createServer((req, rep) => {
-  if (req.url === "/") {
+const server = http.createServer((req, rep) => {
+  if ((req.url = "/")) {
     const page = fs.readFileSync("./index.html");
-    rep.writeHead(200, { "Content-Type": "text/html; UTF-8" });
+    rep.writeHead(200, { "Content-Type": "text/html; UTF-8;" });
     rep.write(page);
     rep.end();
   }
 });
 
-const io = new Server(httpServer);
-
-httpServer.listen(5050, function (error) {
-  console.log("server is runnig");
-  if (error) {
+server.listen(5050, (err) => {
+  console.log("server is runnig . . .");
+  if (err) {
     throw error;
   }
 });
