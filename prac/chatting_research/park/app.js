e57db06fc7a03687d097ef5334ebfab795d27cb9
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-27 11:39:55 +0900

    socket.io 테스트 전 파일 작성

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -1,42 +1,18 @@
 import http from "http";
 import fs from "fs";
 
-const server = http.createServer((req, res) => {
-  if (req.method === "CONNECT") {
-    console.log("연결 요청이 왔습니다.");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("CONNECT 요청 처리 완료!");
-  } else {
-    console.log("GET 요청이 왔습니다.");
-    const page = `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Connect Test</title>
-        </head>
-        <body>
-          <h1>Connect Test</h1>
-          <button id="test">Connect!</button>
-          <script>
-            const button = document.getElementById("test");
-            button.addEventListener("click", async function () {
-              const response = await fetch("http://localhost:5050", {
-                method: "CONNECT",
-              });
-              console.log("응답 받음:", response);
-            });
-          </script>
-        </body>
-      </html>
-    `;
-    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8;" });
-    res.write(page);
-    res.end();
+const server = http.createServer(function (req, rep) {
+  if (req.method === "GET") {
+    const page = fs.readFileSync("./index.html");
+    rep.writeHead(200, { "Content-Type": "text/html; UTF-8" });
+    rep.write(page);
+    rep.end();
   }
 });
 
-server.listen(5050, () => {
-  console.log("서버가 시작되었습니다.");
+server.listen(5050, function (error) {
+  console.log("server is on");
+  if (error) {
+    throw error;
+  }
 });
