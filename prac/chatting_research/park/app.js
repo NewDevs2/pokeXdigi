59e3eb9fb1bfa667d2d0fd2e65aefb743dc776c6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-27 11:57:09 +0900

    오류 수정. 최초 접속을 html이라고 해버렸음

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -2,18 +2,18 @@ import http from "http";
 import fs from "fs";
 
 const server = http.createServer(function (req, rep) {
-  if (req.method === "GET") {
+  if (req.url === "/") {
     const page = fs.readFileSync("./index.html");
     rep.writeHead(200, { "Content-Type": "text/html; UTF-8;" });
     rep.write(page);
-    rep.end();
+    return rep.end();
   }
 
-  if (req.url === "./chat.js") {
+  if (req.url === "/chat.js") {
     const page = fs.readFileSync("./chat.js");
     rep.writeHead(200, { "Content-Type": "text/javascript; UTF-8;" });
     rep.write(page);
-    rep.end();
+    return rep.end();
   }
 });
 
