2a93870bb6846a66952990d7d018e80a8916098d
HaekyungSeong <haekyungs95@gmail.com>
2023-04-30 09:46:22 +0900

    qs 추가해서 입력된 닉네임을 json 파일로 받기를 제작

--- issue/25/seongsocketserver.js
+++ issue/25/seongsocketserver.js
@@ -1,28 +1,44 @@
 // import { Socket } from 'dgram'
 import http from 'http'
-import {Server} from 'socket.io'
-import fs from 'fs'
+import { Server } from 'socket.io'
+import fs, { writeFileSync } from 'fs'
+import qs from 'querystring'
+import { writeFile } from 'fs/promises'
 // import  {socket}  from './seongsockettest.js'
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     if (req.url === '/') {
-      const page = fs.readFileSync('./seongtest.html')
+      const page = fs.readFileSync('./chatstart.html')
       res.writeHead(200, { 'Content-Type': 'text/html' })
       res.write(page)
       res.end()
     }
-    if(req.url.includes('seongsocketclient.js')){
+    if (req.url.includes('seongsocketclient.js')) {
       const page = fs.readFileSync('./seongsocketclient.js')
       res.writeHead(200, { 'Content-Type': 'text/javascript' })
       res.write(page)
       res.end()
     }
-    if(req.url.includes('tagMaker.js')){
+    if (req.url.includes('tagMaker.js')) {
       const page = fs.readFileSync('../../src/models/tag/tagMaker.js')
       res.writeHead(200, { 'Content-Type': 'text/javascript' })
       res.write(page)
       res.end()
+    }
+  } else if (req.method === 'POST') {
+    if (req.url === '/chat') {
+      let datas = ""
+      req.on('data', (data) => {
+        datas += data
+      })
+      req.on('end', () => {
+        writeFileSync('./userid.json', JSON.stringify(qs.parse(datas)))
+        const page = fs.readFileSync('./seongtest.html')
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(page)
+        res.end()
+      })
     }
   }
 }).listen(2080)
@@ -33,7 +49,7 @@ const io = new Server(server)
 
 io.on('connection', (socket) => {
 
-  console.log('새로운 호구 등장',socket.id)
+  console.log('새로운 호구 등장', socket.id)
 
   socket.emit('userid', `${socket.id}`)
   socket.emit('text', `${socket.text}`)
