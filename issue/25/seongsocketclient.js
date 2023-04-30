0d207244fc558b48404b35557c9eb3600e396526
HaekyungSeong <haekyungs95@gmail.com>
2023-04-30 09:45:50 +0900

    url 주소 변경

--- issue/25/seongsocketclient.js
+++ issue/25/seongsocketclient.js
@@ -1,6 +1,6 @@
 import tagMaker from '../../src/models/tag/tagMaker.js'
 
-const socket = io.connect('http://localhost:2080', {
+const socket = io.connect('http://127.0.0.1:2080', {
   path: '/socket.io'
   // transports:['websocket']
   // 처음부터 ws 로 통신할거면 쓰고 안쓰면 폴링연결 먼저 시도
@@ -8,7 +8,7 @@ const socket = io.connect('http://localhost:2080', {
 
 const form = tagMaker('form', document.body);
 const textbox = tagMaker('div', form, {
-  style: 'width: 500px; height:500px; display:flex; flex-flow: column'
+  style: 'width: 300px; height:400px; display:flex; flex-direction: column; align-items:flex-end; '
 })
 const inputtext = tagMaker('input', form, {
   type: 'text',
