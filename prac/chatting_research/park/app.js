5f215fd4b1298bf0ab5d593072e5789b5de9f66f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 11:57:54 +0900

    메세지 전송 확인

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -2,18 +2,31 @@ import http from "http";
 import fs from "fs";
 import { Server } from "socket.io";
 
-const io = new Server(server);
-
-const server = http.createServer((req, rep) => {
+const httpServer = http.createServer((req, rep) => {
   const page = fs.readFileSync("./index.html");
   rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
   rep.write(page);
   rep.end();
 });
 
-server.listen(2080, (err) => {
+const io = new Server(httpServer);
+
+httpServer.listen(2080, (err) => {
   if (err) {
     throw err;
   }
   console.log("server is runnig...");
+});
+
+io.on("connection", (socket) => {
+  console.log("유저가 접속했습니다");
+  socket.on("disconnect", () => {
+    console.log("유저가 나갔습니다.");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
 });
