f370e6ea374de0866be9943fd7906422fe789ed6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 10:43:06 +0900

    기초 파일 생성

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -1,2 +1,16 @@
 import http from "http";
-import fs from "fs ";
+import fs from "fs";
+
+const server = http.createServer((req, rep) => {
+  const page = fs.readFileSync("./index.html");
+  rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
+  rep.write(page);
+  rep.end();
+});
+
+server.listen(2080, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("server is runnig...");
+});
