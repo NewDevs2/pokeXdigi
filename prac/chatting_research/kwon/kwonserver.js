5084fd5312202b9ee5be6f64782cee9e6716f00d
gwonyejun <kyjuni1059@gmail.com>
2023-05-01 16:58:51 +0900

    아이디 값을 가져와서 암호화를 해주면서 쿠키로 보내준다. 그 값을 복호화하여 닉네임으로사용

--- prac/chatting_research/kwon/kwonserver.js
+++ prac/chatting_research/kwon/kwonserver.js
@@ -4,11 +4,33 @@ import qs from "querystring";
 import cryto from "crypto";
 import { Server } from "socket.io";
 
+const algorithm = "aes-256-cbc";
+const password = "my-secret-password";
+const salt = cryto.randomBytes(16);
+const key = cryto.pbkdf2Sync(password, salt, 100000, 32, "sha512");
+const iv = cryto.randomBytes(16);
+
+// 암호화
+function encrypt(text) {
+  const cipher = cryto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return encrypted;
+}
+
+// 복호화
+function decrypt(encrypted) {
+  const decipher = cryto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
 const server = http.createServer((req, res) => {
   switch (req.method) {
     case "GET":
       const ck = fs.readFileSync("./ck.html", "utf-8");
-      
+
       res.setHeader("Content-Type", "text/html");
       res.end(ck);
       break;
@@ -25,18 +47,22 @@ const server = http.createServer((req, res) => {
           // ! id 값이 넘어 오는것을 확인 하였다.
           console.log(postData.id);
           // ! 암호화 하기
-          const hash = cryto.createHash("md5").update(postData.id).digest("hex");
+          // const hash = cryto.createHash('a').update(postData.id).digest("hex");
+          // ! 넘어온 id 값을 암호화 해준다.
+          // const encryptedText = encrypt(postData.id);
+          // console.log("Encrypted text:", encryptedText);
+
+          // const hash =  crypto.randomBytes(16).toString("hex").update(postData.id);
           // console.log(hash);
-          res.setHeader("Set-Cookie", `testCookie=${hash}`);
+          res.setHeader("Set-Cookie", `test=${encrypt(postData.id)}`);
           res.end(client);
           // res.setHeader("Set-Cookie", `testCookie=${postData.id}`);
-         
+
           // res.end(client);
         });
       }
   }
 });
-
 
 const io = new Server(server);
 
@@ -53,25 +79,26 @@ io.on("connection", (socket) => {
     // catch(error){
 
     // }
-
     try {
-   
-        // ! JSON으로 넘어온 값을 객체로 파싱 해준다.
-        const msgParse = JSON.parse(msg);
-        // console.log(socket.apt);
-        // ! 객체로 된 값을 객체 넣어 한번데 클라이언트로 보내어 준다.
-        
-        const objtest = {
-          id: msgParse.id,
-          message: msgParse.message
-        };
-        //  ! 클라이언트에 보내주기 전에 다시 JSON으로 바꾸어 준다.
-        if (msgParse.message !== "") {
+      
+      // ! JSON으로 넘어온 값을 객체로 파싱 해준다.
+      const msgParse = JSON.parse(msg);
+      // console.log(socket.apt);
+      // ! 암호화된 값을 복호화를 통해 내보낸다.
+      // const decryptedText = decrypt(msgParse.id);
+      // console.log("Decrypted text:", decryptedText);
+
+      // ! 객체로 된 값을 객체 넣어 한번데 클라이언트로 보내어 준다.
+      const objtest = {
+        id: decrypt(msgParse.id),
+        message: msgParse.message,
+      };
+      //  ! 클라이언트에 보내주기 전에 다시 JSON으로 바꾸어 준다.
+      if (msgParse.message !== "") {
         const js = JSON.stringify(objtest);
         // console.log(msg)
-        console.log(  objtest );
-        io.emit("chat message",js);
-        
+        console.log(objtest);
+        io.emit("chat message", js);
       } else {
         alert("공백을 입력 하셨습니다.");
       }
