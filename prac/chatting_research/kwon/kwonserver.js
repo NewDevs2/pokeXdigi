d9786fab17ea9f4ed85f8b3114679e01ccd42df3
gwonyejun <kyjuni1059@gmail.com>
2023-05-01 12:36:08 +0900

    쿠키를 사용 하여 아이디 값으로 접속을 완료 하였다.

--- prac/chatting_research/kwon/kwonserver.js
+++ prac/chatting_research/kwon/kwonserver.js
@@ -1,12 +1,36 @@
 import http from "http";
 import fs from "fs";
+import qs from "querystring";
 import { Server } from "socket.io";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  const apt = '권';
-  res.end(fs.readFileSync("./kwonclient.html", "utf-8"));
+  switch (req.method) {
+    case "GET":
+      const ck = fs.readFileSync("./ck.html", "utf-8");
+      const cookieValue = "test-cookie-value";
+      res.setHeader("Content-Type", "text/html");
+      res.end(ck);
+      break;
+    case "POST":
+      if (req.url === "/login") {
+        let data = "";
+        req.on("data", (chunk) => {
+          data += chunk;
+        });
+        req.on("end", () => {
+          const postData = qs.parse(data);
+          const client = fs.readFileSync("./kwonclient.html", "utf-8");
+          res.setHeader("Content-Type", "text/html");
+          // ! id 값이 넘어 오는것을 확인 하였다.
+          res.setHeader("Set-Cookie", `testCookie=${postData.id}`);
+          console.log(postData.id);
+          res.end(client);
+        });
+      }
+  }
 });
+
+
 const io = new Server(server);
 
 io.on("connection", (socket) => {
