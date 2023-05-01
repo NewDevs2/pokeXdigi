e3c63a60e0f624b0a0154300122c0703633747bf
jangrubin <wkdfnqls2465@gmail.com>
2023-05-01 10:35:11 +0900

    채팅 + json 데이터 교환 구현

--- prac/chatting_research/jang/test3.js
+++ prac/chatting_research/jang/test3.js
@@ -1,3 +1,70 @@
+import http from "http";
+// ? socket.io의 Server 클래스 가져오기
+import { Server } from "socket.io";
+import fs from "fs";
+import { Console } from "console";
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(fs.readFileSync("./test.html", "utf-8"));
+});
+// console.log(Server)
+
+// !소켓 서버 생성
+// 웹 서버와 소켓 서버
+// ? Server 클래스 새 인스턴스 생성
+const io = new Server(server);
+// 내장 이벤트 connection을 실행
+io.on("connection", (socket) => {
+  // console.log("a user connected");
+  // 메시지가 왔을 때
+  // console.log(socket)
+  socket.on("send info", (data) => {
+    // console.log(data.msg);
+    // JSON.parse(data);
+    // console.log(data);
+    const beforeJsonData = JSON.parse(data)
+    // console.log(jsonData)
+    const jsonData = JSON.stringify(beforeJsonData);
+    // console.log(jsonData);
+    // 클라이언트에게 받은 데이터 다시 보내줌
+    io.emit("send info", jsonData);
+    // JSON.parse(msg)
+    // console.log("sendInfo: " + data);
+    // io.emit("sendInfo", msg);
+  });
+  // 아이디가 들어왔을 때
+  // socket.on("user id", (userId) => {
+  //   JSON.parse(userId);
+  //   console.log("user id" + userId);
+  //   io.emit("user id", userId);
+  // });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(2222, (err) => {
+  if (err) {
+    return console.log("연결이 원할하지 않습니다.", err);
+  }
+  console.log("성공");
+});
+// import http from 'http';
+// import { Server } from 'socket.io';
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.end('Hello World\n');
+// });
+// const io = new Server(server);
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+// server.listen(2222, () => {
+//   console.log('Server is listening on port 2222');
+// });
+
 // import http from "http";
 // import {Server} from "socket.io";
 // import fs from "fs";
@@ -50,64 +117,3 @@
 // server.listen(8080,()=> {
 //   console.log("정상 연결 됨")
 // })
-
-import http from "http";
-import { Server } from "socket.io";
-import fs from "fs";
-import { Console } from "console";
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(fs.readFileSync("./test.html", "utf-8"));
-});
-// !소켓 서버 생성
-// 웹 서버와 소켓 서버를 구축
-const io = new Server(server);
-// 내장 이벤트 connection을 실행
-io.on("connection", (socket) => {
-  // console.log("a user connected");
-  // 메시지가 왔을 때
-  socket.on("send info", (data) => {
-    // console.log(data.msg);
-    // JSON.parse(data);
-    // console.log(data);
-    const beforeJsonData = JSON.parse(data)
-    // console.log(jsonData)
-    const JsonData = JSON.stringify(beforeJsonData);
-    // console.log(data);
-    io.emit("send info", JsonData);
-    // JSON.parse(msg)
-    // console.log("sendInfo: " + data);
-    // io.emit("sendInfo", msg);
-  });
-  // 아이디가 들어왔을 때
-  // socket.on("user id", (userId) => {
-  //   JSON.parse(userId);
-  //   console.log("user id" + userId);
-  //   io.emit("user id", userId);
-  // });
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-server.listen(2222, (err) => {
-  if (err) {
-    return console.log("something bad happened", err);
-  }
-  console.log("성공");
-});
-// import http from 'http';
-// import { Server } from 'socket.io';
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.end('Hello World\n');
-// });
-// const io = new Server(server);
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
-// });
-// server.listen(2222, () => {
-//   console.log('Server is listening on port 2222');
-// });
