7bcf5e1258171d3406a7152ba3843384a0da92f4
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 14:12:24 +0900

    재작성

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -1,39 +1,18 @@
 import http from "http";
 import fs from "fs";
-import { Server } from "socket.io";
 
-const httpServer = http.createServer((req, rep) => {
+const httpServer = http.createServer(function (req, rep) {
   if (req.url === "/") {
-    const page = fs.readFileSync("./login.html");
-    rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
-    rep.write(page);
-    rep.end();
-  }
-  if (req.url === "/chat.html") {
-    const page = fs.readFileSync("./chat.html");
-    rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
+    const page = fs.readFileSync("./index.html");
+    rep.writeHead(200, { "Content-Type": "text/html; charset:utf-8" });
     rep.write(page);
     rep.end();
   }
 });
-
-const io = new Server(httpServer);
 
 httpServer.listen(2080, (err) => {
   if (err) {
     throw err;
   }
-  console.log("server is runnig...");
-});
-
-io.on("connection", (socket) => {
-  console.log("유저가 접속했습니다");
-  socket.broadcast.emit("안녕하세요.");
-  socket.on("disconnect", () => {
-    console.log("유저가 나갔습니다.");
-  });
-  socket.on("chat message", (msg) => {
-    console.log("입력된 메세지: " + msg);
-    io.emit("chat message", msg);
-  });
+  console.log("server is runnig");
 });
