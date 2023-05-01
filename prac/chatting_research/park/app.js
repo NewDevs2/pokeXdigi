b5bedd7b692feb9f59760c65527ac021da99465f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 14:52:28 +0900

    post 수정

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -28,11 +28,18 @@ httpServer.listen(2080, (err) => {
 
 io.on("connection", (socket) => {
   console.log("유저가 접속했습니다");
+  console.log(socket.id);
   socket.broadcast.emit("안녕하세요.");
+  let setNick = "";
   socket.on("disconnect", () => {
     console.log("유저가 나갔습니다.");
   });
+  socket.on("setNickname", (nickname) => {
+    setNick = nickname;
+    return setNick;
+  });
   socket.on("chat message", (msg) => {
+    console.log(setNick);
     console.log("입력된 메세지: " + msg);
     io.emit("chat message", msg);
   });
