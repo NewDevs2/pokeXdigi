c02dbf876da6a0469538ce0106782b8f216bfaee
HaekyungSeong <haekyungs95@gmail.com>
2023-05-01 17:23:09 +0900

    소캣 하나 더 만들깅

---
+++ issue/26/seong/socketserver.js
@@ -0,0 +1,40 @@
+import http from 'http'
+import {Server} from 'socket.io'
+import fs from 'fs'
+
+const server = http.createServer((req,res)=>{
+  if(req.method==='GET'){
+    if(req.url==='/'){
+      const page = fs.readFileSync('./index.html')
+      res.writeHead(200, {'Content-Type':'text/html'})
+      res.write(page)
+      res.end()
+    }
+  }
+  if(req.method==='POST'){
+    if(req.url==='/chatting'){
+      const page = fs.readFileSync('./chatting.html')
+      
+      res.writeHead(200,{'Content-Type':'text/html'})
+      res.write(page)
+      res.end()
+    }
+  }
+}).listen(2080)
+
+const io = new Server(server);
+
+io.on('connection',(socket)=>{
+  io.emit('connectted',socket.id)
+
+  socket.on("disconnect",()=>{
+    console.log("잘가~")
+    io.emit("disconnected",socket.id)
+  })
+
+  socket.on("chat",(data)=>{
+    console.log(data)
+    socket.broadcast.emit("chat",data)
+  })
+
+})
