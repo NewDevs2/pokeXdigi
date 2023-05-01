532b6840b90fc50085ce1604e2fcbb23b4b81bf8
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 16:46:12 +0900

    거의 다 옴

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -9,8 +9,7 @@ const httpServer = http.createServer((req, rep) => {
     rep.write(page);
     rep.end();
   }
-  console.log(req.headers);
-  if (req.url === "/chat.html" && req.method === "POST") {
+  if (req.url === "/chat" && req.method === "POST") {
     const page = fs.readFileSync("./chat.html");
     rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
     rep.write(page);
@@ -27,19 +26,19 @@ httpServer.listen(2080, (err) => {
   console.log("server is runnig...");
 });
 
+let setNick = "";
 io.on("connection", (socket) => {
-  console.log("유저가 접속했습니다");
-  let setNick = "";
+  // console.log("유저가 접속했습니다");
   console.log(socket.id);
   // setNick = socket.id;
-  socket.broadcast.emit("안녕하세요.");
+  // socket.broadcast.emit("안녕하세요.");
   socket.on("disconnect", () => {
     console.log("유저가 나갔습니다.");
   });
   socket.on("setNickname", (nickname) => {
     setNick = nickname;
-    console.log(nickname);
-    // return setNick;
+    console.log(`form에 입력된 값 : ${setNick}`);
+    return setNick;
   });
   socket.on("chat message", (msg) => {
     // console.log(setNick);
