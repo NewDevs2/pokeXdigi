57d1d9ace01a4b0f99a2d1143b94c8bdc73ed4af
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-01 12:50:32 +0900

    닉네임 시도

--- prac/chatting_research/park/app.js
+++ prac/chatting_research/park/app.js
@@ -3,10 +3,18 @@ import fs from "fs";
 import { Server } from "socket.io";
 
 const httpServer = http.createServer((req, rep) => {
-  const page = fs.readFileSync("./index.html");
-  rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
-  rep.write(page);
-  rep.end();
+  if (req.url === "/") {
+    const page = fs.readFileSync("./login.html");
+    rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
+    rep.write(page);
+    rep.end();
+  }
+  if (req.url === "/chat.html") {
+    const page = fs.readFileSync("./chat.html");
+    rep.writeHead(200, { "Content-Type": "text/html; charset:UTF-8;" });
+    rep.write(page);
+    rep.end();
+  }
 });
 
 const io = new Server(httpServer);
@@ -29,5 +37,3 @@ io.on("connection", (socket) => {
     io.emit("chat message", msg);
   });
 });
-
-// socket.broadcast.emit("hi");
