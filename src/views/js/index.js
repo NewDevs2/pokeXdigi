a337bf21f6f186180e0928f469cea148f239b6f3
KwakYounHo <kwakyh951@gmail.com>
2023-05-02 16:40:59 +0900

    색종이 z인덱스 실험

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -2,22 +2,24 @@
 import tagMaker from "../../models/tag/tagMaker.js";
 
 // ! body 태그와 같은 크기를 준다.
-const wrap = tagMaker("div", document.body, {
-  id: "wrap",
-  style:
-    " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
-});
+const wrap = tagMaker("div", document.body, {id: "wrap"});
+const colorPaper = tagMaker("div", wrap, {className:"colorPaper"})
+
+const firstScreen  = tagMaker("div", wrap, {id:"firstScreen"});
+const secondScreen = tagMaker("div", wrap, {id:"secondScreen"});
+const thirdScreen  = tagMaker("div", wrap, {id:"thirdScreen"});
+
 
 //! 테스트
-const colorPaper = tagMaker('div', wrap, {id : "colorPaper"});
-const container  = tagMaker('div', wrap, {id : "container"});
+// const colorPaper = tagMaker('div', wrap, {id : "colorPaper"});
+// const container  = tagMaker('div', wrap, {id : "container"});
 
 //! 버튼 만들기
-const btnback = tagMaker("button", container, { innerText: "sing-in" });
+const btnback = tagMaker("button", firstScreen, { innerText: "sing-in" });
 btnback.addEventListener("click", function () {
   location.href = "/src/views/html/login.html";
 });
-const btnback2 = tagMaker("button", container, { innerText: "sing-up" });
+const btnback2 = tagMaker("button", firstScreen, { innerText: "sing-up" });
 btnback2.addEventListener("click", function () {
   location.href = "/src/views/html/createAccount.html";
 });
