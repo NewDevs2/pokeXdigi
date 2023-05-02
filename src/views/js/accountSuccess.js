98aeaaa7d0920cfb997ce09c091d5b36ede10435
jangrubin <wkdfnqls2465@gmail.com>
2023-05-02 12:03:05 +0900

    공용 css 기반 스타일 수정

--- src/views/js/accountSuccess.js
+++ src/views/js/accountSuccess.js
@@ -3,39 +3,45 @@ import tagMaker from "../../models/tag/tagMaker.js"
 // wrap = body
 tagMaker("div", document.body, {
   id:"wrap",
-  style:"width:1920px; height:1080px;  background-color:gray;display:flex;justify-content:center;"
+  style:"display:flex;justify-content:center;"
 })
+// 컬러 페이퍼
+const colorPaper = tagMaker("div", wrap, {
+  id:"colorPaper"
+});
 // 컨테이너
 const container = tagMaker("div", wrap, {
   id:"container",
-  style:"width:45%; height:100%; background-color:white;display:flex;justify-content:center;align-items:center; "
+  style:"width:660px; height:100%; background-color:white;display:flex;justify-content:center;align-items:center; "
 })
 // 박스
 const box = tagMaker("div", container, {
   id:"box",
-  style:"width:100%; height:100%; display:flex; justify-content:space-evenly;align-items:center; flex-direction:column;"
+  style:"width:100%; height:640px; display:flex; justify-content:space-evenly;align-items:center; flex-direction:column;"
 })
 // 박스의 자식 요소들
 const checkImg =tagMaker("div",box,{
   id:"checkImg",
-  style:"width:400px; height:400px;background-color:gray; border-radius: 50%;"
+  style:"width:350px; height:350px; border-radius: 50%;background-color:#FCA311;display:flex; align-items:center; justify-content:center;"
 })
-// tagMaker("div",checkImg,{
-//   style:'display: block;width: 40px;height: 40px;border: 4px solid white;border-radius: 50%;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%) rotate(45deg);'
-// })
+// 체크모양 이미지
+tagMaker("div",checkImg,{
+  style : "width : 270px; height : 210px;background-image : url('/src/views/img/WhiteCheckMark.png');background-size: cover;"
+})
 
 tagMaker("div",box,{
   innerHTML:"계정이 생성되었습니다!",
-  style:"font-size:30px; display:flex; align-items:center;"
+  style:"font-size:56px; display:flex; align-items:center;;"
 })
 tagMaker("div",box,{
-  innerHTML:"지금 바로 PokeDigi를 이용해보세요."
+  innerHTML:"지금 바로 PokeDigi를 이용해보세요.",
+  style: "font-size: 28px;"
 })
 const loginButton = tagMaker("input",box,{
   type:"button",
   value:"로그인",
   id:"login",
-  style:"width:500px; height:100px; font-size:40px;"
+  style:"width:500px; height:100px; font-size:40px; background-color:#FCA311; color:white;"
 })
 // 로그인 버튼 이벤트
 loginButton.addEventListener("click", () => {
