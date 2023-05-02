7485456a99c7e875c8a30a462bfbf205f185827f
gwonyejun <kyjuni1059@gmail.com>
2023-05-02 16:41:26 +0900

    색종이 연습 브랜치

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,23 +1,31 @@
-
 import tagMaker from "../../models/tag/tagMaker.js";
 
 // ! body 태그와 같은 크기를 준다.
 const wrap = tagMaker("div", document.body, {
   id: "wrap",
-  style:
-    " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
 });
-
+const wrap1 = tagMaker("div", wrap, {
+  id: "wrap1",
+});
+tagMaker("div", wrap1, {
+  id: "test",
+});
+tagMaker("div", wrap, {
+  id: "wrap2",
+});
+tagMaker("div", wrap, {
+  id: "wrap3",
+});
 //! 테스트
-const colorPaper = tagMaker('div', wrap, {id : "colorPaper"});
-const container  = tagMaker('div', wrap, {id : "container"});
+// const colorPaper = tagMaker('div', wrap, {id : "colorPaper"});
+// const container  = tagMaker('div', wrap, {id : "container"});
 
 //! 버튼 만들기
-const btnback = tagMaker("button", container, { innerText: "sing-in" });
+const btnback = tagMaker("button", wrap1, {id: 'btn', innerText: "sing-in" });
 btnback.addEventListener("click", function () {
   location.href = "/src/views/html/login.html";
 });
-const btnback2 = tagMaker("button", container, { innerText: "sing-up" });
+const btnback2 = tagMaker("button", wrap1, { innerText: "sing-up" });
 btnback2.addEventListener("click", function () {
   location.href = "/src/views/html/createAccount.html";
 });
