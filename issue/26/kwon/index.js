fab41efb42705fff35d07fb680c4f300e3ae9db1
gwonyejun <kyjuni1059@gmail.com>
2023-05-03 12:54:16 +0900

    db접속을 못하여 나중에 에러 검토 해보아야 한다. 개인적으로

---
+++ issue/26/kwon/index.js
@@ -0,0 +1,23 @@
+
+import tagMaker from "../../models/tag/tagMaker.js";
+
+// ! body 태그와 같은 크기를 준다.
+const wrap = tagMaker("div", document.body, {
+  id: "wrap",
+  style:
+    " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
+});
+
+//! 테스트
+const colorPaper = tagMaker('div', wrap, {id : "colorPaper"});
+const container  = tagMaker('div', wrap, {id : "container"});
+
+//! 버튼 만들기
+const btnback = tagMaker("button", container, { innerText: "sing-in" });
+btnback.addEventListener("click", function () {
+  location.href = "/src/views/html/login.html";
+});
+const btnback2 = tagMaker("button", container, { innerText: "sing-up" });
+btnback2.addEventListener("click", function () {
+  location.href = "/src/views/html/createAccount.html";
+});
