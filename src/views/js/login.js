9317dbf73ae8a94934dcb99a165a25ba8853aded
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-03 16:10:52 +0900

    login.js, loginfail.js 오타 수정

--- src/views/js/login.js
+++ src/views/js/login.js
@@ -1,22 +1,22 @@
-import tagMaker from '../../models/tag/tagMaker.js'
+import tagMaker from "../../models/tag/tagMaker.js";
 
 // * 태그를 만드는 함수
 // const MKTag = (tagName, target, Attribute, innerText) => {
-  // const tag = document.createElement(tagName);
-  // target.appendChild(tag);
-  // if (Attribute && typeof Attribute === "object") {
-    // for (p in Attribute) {
-      // tag.setAttribute(p, Attribute[p]);
-    // }
-  // }
-  // if (innerText) {
-    // tag.innerText = innerText;
-  // }
+// const tag = document.createElement(tagName);
+// target.appendChild(tag);
+// if (Attribute && typeof Attribute === "object") {
+// for (p in Attribute) {
+// tag.setAttribute(p, Attribute[p]);
+// }
+// }
+// if (innerText) {
+// tag.innerText = innerText;
+// }
 // };
 
-const wrap       = tagMaker('div',document.body, {id:'wrap'});
-const colorPaper = tagMaker('div', wrap, {id:'colorPaper'});
-const container  = tagMaker('div', wrap, {id:'container'});
+const wrap = tagMaker("div", document.body, { id: "wrap" });
+const colorPaper = tagMaker("div", wrap, { id: "colorPaper" });
+const container = tagMaker("div", wrap, { id: "container" });
 
 // * 컨테이너 영역 분할
 for (let i = 0; i < 4; i++) {
@@ -31,11 +31,11 @@ const login_button = container.children[3];
 // login_logo.innerHTML = '<h3 style="color:gray;">로고 들어갈 곳</h3>';
 
 function makeLoginForm() {
-  const logoP = tagMaker("p", login_logo)
-  logoP.innerText = 'POKE X DIGI'
+  const logoP = tagMaker("p", login_logo);
+  logoP.innerText = "POKE X DIGI";
 
   //* h1 생성
-  tagMaker("h1", login_form, {innerText:"로그인"});
+  tagMaker("h1", login_form, { innerText: "로그인" });
 
   //* 폼 태그 생성
   let Attri = {
@@ -87,24 +87,16 @@ function makeLoginForm() {
   //* 계정 찾기 / 계정
   for (let i = 0; i < 2; i++) {
     if (i === 0) {
-      tagMaker(
-        "a",
-        login_findORJoin,
-        {
-          href: "/src/views/html/findAccount.html",
-          innerText:"계정을 잊으셨나요?"
-        }
-      );
+      tagMaker("a", login_findORJoin, {
+        href: "/src/views/html/findAccount.html",
+        innerText: "계정을 잊으셨나요?",
+      });
     }
     if (i === 1) {
-      tagMaker(
-        "a",
-        login_findORJoin,
-        {
-          href: "/src/views/html/createAccount.html",
-          innerText: "계정생성"
-        }
-      );
+      tagMaker("a", login_findORJoin, {
+        href: "/src/views/html/createAccount.html",
+        innerText: "계정 생성",
+      });
     }
     form.appendChild(login_findORJoin);
     form.appendChild(login_button);
