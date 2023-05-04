9be405f701778731a062a9049c04760a7f55e2b8
gwonyejun <kyjuni1059@gmail.com>
2023-05-04 11:53:42 +0900

    XML방식으로 요청을 보내어서 값을 가져오고 싶다 포켓몬 api를 사용했을 때 처럼

--- src/views/js/testrep.js
+++ src/views/js/testrep.js
@@ -1,5 +1,26 @@
 // window.lo
-const fetchtest = fetch('/checkCookie');
+const fetchtest = fetch("/checkCookie")
+  .then((response) => {
+    // 서버 응답을 받았을 때 처리할 로직
+    //   const data = response.json();
+    //  console.log(data);
+
+    console.log(response);
+  })
+  .catch((error) => {
+    // 서버 요청이 실패했을 때 처리할 로직
+    console.error(error);
+  });
+const xhr = new XMLHttpRequest();
+const _URL = "<http://localhost:8080/checkCookie>";
+
+xhr.open("GET", _URL);
+xhr.send();
+xhr.addEventListener("load", function () {
+  // console.log(JSON.parse(xhr.response));
+  const cookieJsonData = JSON.parse(xhr.response);
+  console.log(cookieJsonData);
+});
 console.log(fetchtest);
 
 // fetch('/checkCookie')
