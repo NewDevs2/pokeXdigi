8824b2dbba91c64cbad7cd4847ef7f033430ca4d
gwonyejun <kyjuni1059@gmail.com>
2023-05-04 11:09:48 +0900

    인덱스문서에서 쿠키를 가져오기 위해 파일 요청을 하나더 보내었다.

---
+++ src/views/js/testrep.js
@@ -0,0 +1,8 @@
+// window.lo
+const fetchtest = fetch('/checkCookie');
+console.log(fetchtest);
+
+// fetch('/checkCookie')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
