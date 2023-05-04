91668e445a58abf1fbe513242457e4a462f8dfd9
HaekyungSeong <haekyungs95@gmail.com>
2023-05-04 11:26:38 +0900

    쿠키 데이터를 json 화 하여 다른 페이지의 데이터로 사용하기 성공

--- prac/cookie_research/seong/socketserver.js
+++ prac/cookie_research/seong/socketserver.js
@@ -1,35 +1,40 @@
 import http from 'http'
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 import fs from 'fs'
 import qs from 'querystring'
 
-const server = http.createServer((req,res)=>{
-  if(req.method==='GET'){
-    if(req.url==='/'){
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
       const page = fs.readFileSync('./index.html')
-      res.writeHead(200, {'Content-Type':'text/html'})
+      res.writeHead(200, { 'Content-Type': 'text/html' })
       res.write(page)
+      res.end()
+    } else if (req.url.includes('/enter')) {
+      res.writeHead(200, { 'Content-Type': 'text/JSON' })
+      res.write(JSON.stringify(decodeURIComponent(req.headers.cookie)))
       res.end()
     }
   }
-  if(req.method==='POST'){
-    if(req.url==='/chatting'){
-      let names=''
-      req.on('data',(data)=>{
-        names +=data
+  if (req.method === 'POST') {
+    if (req.url.includes('/chatting')) {
+      let names = ''
+      req.on('data', (data) => {
+        names += data
       })
-      req.on('end',()=>{
-        const user=decodeURIComponent(names);
+      req.on('end', () => {
+        const user = decodeURIComponent(names);
         // qs.parse(names)
         console.log(user)
         // console.log(Object.keys(user))
 
         const page = fs.readFileSync('./chatting.html')
-        
-        // ! 쿠키에는 ASCII 코드 외에 넣을 수 없음. 넣고 싶으면 encode 해서 넣어야 한다. = 한글안됨
-        res.writeHead(200,{'Content-Type':'text/html','Set-Cookie':`${encodeURI(user)}; HttpOnly`})
 
-        console.log(decodeURIComponent(req.headers.cookie))
+        // ! 쿠키에는 ASCII 코드 외에 넣을 수 없음. 넣고 싶으면 encode 해서 넣어야 한다. = 한글안됨
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Set-Cookie': `${encodeURI(user)}; HttpOnly` })
+
+        // let usercookie = qs.parse(decodeURIComponent(req.headers.cookie))
+
         res.write(page)
         res.end()
       })
@@ -39,17 +44,21 @@ const server = http.createServer((req,res)=>{
 
 const io = new Server(server);
 
-io.on('connection',(socket)=>{
-  io.emit('connectted',socket.id)
+io.on('connection', (socket) => {
+  io.emit('connectted', socket.id)
 
-  socket.on("disconnect",()=>{
+  socket.on("disconnect", () => {
     console.log("잘가~")
-    io.emit("disconnected",socket.id)
+    io.emit("disconnected", socket.id)
   })
 
-  socket.on("chat",(data)=>{
+  socket.on("userid",(data)=>{
+    socket.userid = data;
+  })
+
+  socket.on("chat", (data) => {
     console.log(data)
-    socket.broadcast.emit("chat",data)
+    socket.broadcast.emit("chat", data)
   })
 
 })
