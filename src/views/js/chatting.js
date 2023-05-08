3b9272f2618f3d9f1c41886b7bffcd85680e6e67
jangrubin <wkdfnqls2465@gmail.com>
2023-05-08 18:01:31 +0900

    tagMaker 모듈 호출 및 태그 생성

---
+++ src/views/js/chatting.js
@@ -0,0 +1,11 @@
+import tagMaker from "../../models/tag/tagMaker.js"
+// const colorPaper = tagMaker("div", wrap, { id: "colorPaper" });
+const wrap = tagMaker("div",document.body, {
+  id : "wrap"
+});
+const container = tagMaker("div", wrap, {
+  id : "container"
+});
+const menuBar = tagMaker("div", wrap, {
+  id : "menuBar"
+})
