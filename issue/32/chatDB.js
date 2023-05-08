32ec622d76feadefdff8e94f6b7e75ecc41aeb52
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-08 17:55:17 +0900

    기초 파일 작성, db 연결 테스트

---
+++ issue/32/chatDB.js
@@ -0,0 +1,14 @@
+import http from "http";
+import fs from "fs";
+import qs from "querystring";
+import path from "path";
+import { fileURLToPath } from "url";
+import sign_master from "../../src/models/DBConfig.js";
+import responseModule from "../../issue/21/responseModule.js";
+
+sign_master.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("DB 연결");
+});
