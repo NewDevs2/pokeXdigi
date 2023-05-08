28ec25a114ffab9be6c50865c778eaec0df3b9e1
gwonyejun <kyjuni1059@gmail.com>
2023-05-08 20:24:32 +0900

    채팅 html 작업 Fixes #31

---
+++ src/views/js/testbtn.js
@@ -0,0 +1,40 @@
+import tagMaker from "../../models/tag/tagMaker.js";
+
+
+
+
+const wrap = tagMaker("div", document.body, {
+  id: "wrap",
+});
+
+//! 테스트
+
+const container = tagMaker("div", wrap, { id: "container" });
+const menuBar = tagMaker("div", wrap, { id: "menuBar" });
+
+
+//! 버튼 만들기
+const btnback = tagMaker("button", menuBar, { className: "chat" });
+const btnImg = tagMaker("img", btnback, {src:"https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800"});
+btnback.addEventListener("click", function () {
+  let togle = true;
+
+  if (togle === true) {
+
+    const chattingBar = tagMaker("div", container, { id: "chattingBar" });
+    togle = false;
+
+    console.log(togle);
+  }
+  else if(togle === false){
+    // const chattingNone = document.getElementById("chattingNone");
+    // chattingNone.style.display = "none";
+    
+    // container.removeChild()
+    togle = true;
+    console.log(togle);
+  }
+    
+
+})
+
