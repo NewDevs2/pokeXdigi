4daf0e4a0e07afd40b1bb5e8f96ad20d9e64388b
gwonyejun <kyjuni1059@gmail.com>
2023-05-08 22:14:54 +0900

    토글 방식으로 채팅바를 생기게 하였다.

--- src/views/js/testbtn.js
+++ src/views/js/testbtn.js
@@ -16,8 +16,8 @@ const menuBar = tagMaker("div", wrap, { id: "menuBar" });
 //! 버튼 만들기
 const btnback = tagMaker("button", menuBar, { className: "chat" });
 const btnImg = tagMaker("img", btnback, {src:"https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800"});
+let togle = true;
 btnback.addEventListener("click", function () {
-  let togle = true;
 
   if (togle === true) {
 
@@ -27,10 +27,10 @@ btnback.addEventListener("click", function () {
     console.log(togle);
   }
   else if(togle === false){
-    // const chattingNone = document.getElementById("chattingNone");
+    const chattingNone = document.getElementById("chattingNone");
     // chattingNone.style.display = "none";
     
-    // container.removeChild()
+    container.removeChild(container.firstElementChild);
     togle = true;
     console.log(togle);
   }
