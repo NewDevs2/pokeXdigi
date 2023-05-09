e58021e9e68e7c3b18b6c05b11072e9a92d6c977
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-09 10:16:02 +0900

    sign_master 권한 수정 필요

--- issue/32/chatDB.js
+++ issue/32/chatDB.js
@@ -13,3 +13,14 @@ sign_master.connect(function (err) {
   console.log("DB 연결");
 });
 
+sign_master.query(
+  `INSERT INTO chatting_log(LOG_NUM, ID, CHATTING_LOG) values (2, KRAPLI, '{"name": "John", "age": 30, "city": "New York"}')`,
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+// JSON 유형
+// {"name":"john", "age": 30, "city":"New York"}
