e810e473c8911663135630e514dc73df24e842e6
박준형 <108733795+krapli441@users.noreply.github.com>
2023-05-09 16:31:59 +0900

    chat.js ip 수정

--- src/views/js/chat.js
+++ src/views/js/chat.js
@@ -5,7 +5,7 @@ import { sendCookie } from "../../../utils/Cookie/cookieManager.js";
 
 window.onload = (() => {
   // 소켓 서버 접속
-  const socket = io("192.168.12.243:8080", { path: "/chat/" });
+  const socket = io("192.168.100.124:8080", { path: "/chat/" });
 
   // 채팅 서버에 최초 접속 시 유저의 nickname을 쿠키에서 가져와 전송한다
   sendCookie((cookieData) => {
