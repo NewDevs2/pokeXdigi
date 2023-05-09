17bfaa9c1ba4a7dec15dde34416cb66141aedf79
jangrubin <wkdfnqls2465@gmail.com>
2023-05-09 17:45:17 +0900

    예준씨 브랜치와 병합, 충돌 제거 버그 해결

--- src/views/js/testbtn.js
+++ src/views/js/testbtn.js
@@ -5,7 +5,7 @@ const uid = "test";
 //   id: "wrap",
 // });
 
-//! 테스트
+// //! 테스트
 
 // const container = tagMaker("div", wrap, { id: "container" });
 // const menuBar = tagMaker("div", wrap, { id: "menuBar" });
@@ -24,26 +24,27 @@ chattingBar.style.display = "none";
 const chattingHeader = tagMaker("div", chattingBar);
 // ! 안 보이지만 영역을 3등분으로 나누어 주았다.
 tagMaker("div", chattingHeader);
-tagMaker("div", chattingHeader);
-const closeBtn = tagMaker("button", chattingHeader,{className: "fa-solid fa-x"});
-
+tagMaker("h2", chattingHeader , {innerText:`${uid}`});
+const closeBtn = tagMaker("button", chattingHeader,{className: "fa-x fa-2x"});
+{/* <i class="fa-solid fa-xmark"></i> */}
 // ! 채팅방 내용 보여지는 공간
-tagMaker("div", chattingBar, { id: "chatBox" });
+tagMaker("div", chattingBar,{id:"chatBox"});
+
 
 // ! 채팅을 치는 공간과 채팅을 치면서 서버에게 데이터를 넘길수 있도록 해준다.
-const chattingForm = tagMaker("form", chattingBar, {
-  id: "chattingForm",
-});
-tagMaker("input", chattingForm, { type: "text" });
-tagMaker("button", chattingForm, { type: "submit", innerText: "전송" });
+const chattingForm = tagMaker("form", chattingBar ,{id: "chattingForm" ,action: "",method:"POST"});
+tagMaker("input", chattingForm,{type:"text"});
+tagMaker("button", chattingForm,{type:"submit", innerText: "전송",});
 
 let togle = true;
 togleBtn.addEventListener("click", function () {
+
   if (togle === true) {
     chattingBar.style.display = "block";
-
+    
+    
     togle = false;
-
+    
     console.log(togle);
   } else if (togle === false) {
     chattingBar.style.display = "none";
@@ -56,7 +57,8 @@ togleBtn.addEventListener("click", function () {
 
 closeBtn.addEventListener("click", function () {
   togle = true;
-  chattingBar.style.display = "none";
+    chattingBar.style.display = "none";
+
 });
 
 // togleBtn.addEventListener("click", function () {
