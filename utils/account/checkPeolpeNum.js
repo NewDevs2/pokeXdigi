64ad83528cd3b1337465354d9615ed962a09cb20
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 20:51:39 +0900

    정규식 수정

--- utils/account/checkPeolpeNum.js
+++ utils/account/checkPeolpeNum.js
@@ -1,5 +1,11 @@
 // export default
  function test(clientInput) {
+    // [0-9]0~9까지의 숫자
+    //  {6}자리와
+    // [- ]? 0~1개의 공백
+    // [1-4] 1~4까지의 성별 나타내는 숫자
+    // [0-9]$ 0~9 숫자 {6자리의 숫자} -> 고유 번호
+    // $ 문자의 끝
     let checkForm = /^[0-9]{6}[- ]?[1-4][0-9]{6}$/;
     console.log(checkForm.test(clientInput))
     // return checkForm.test(clientInput);
