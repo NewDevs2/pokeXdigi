e5dbc74fed6ae9d5a66e4e9bcc5e14536a956021
krapli441 <tlfqj1208@gmail.com>
2023-05-18 22:02:25 +0900

    index.js : 스크린 영역을 세 개로 나누기 위해 tagMaker 모듈을 이용하여 wrapSecond, wrapThird를 제작하였음. 그리고, 마우스 휠 스크롤에 대응하는 handleMouseWheel 함수를 GPT 도움으로 제작함.
    main.css : index.js에서 생성된 wrapSecond, wrapThird에 맞는 css 스타일링 속성을 입력함.
    wrap, wrapSecond,wrapThird는 z-index 속성을 부여하여 div의 높낮이(?)를 설정해주었음. (다른 div가 간섭받지 않도록 함)
    #container 영역을 삭제하였고 wrap안에 POKE X DIGI h1 태그를 넣었음.
    #buttons 스타일링 작업을 진행함. 색깔 변경, 크기 변경, 호버 애니메이션을 추가하였음.

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -31,6 +31,7 @@ sendCookie((data) => {
     });
 
     //! 테스트
+
     const colorPaper = tagMaker("div", wrap, { id: "colorPaper" });
     const container = tagMaker("div", wrap, { id: "container" });
     const btnback = tagMaker("button", container, {
@@ -51,8 +52,26 @@ sendCookie((data) => {
         " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
     });
 
+    const wrapSecond = tagMaker("div", document.body, {
+      id: "wrap",
+      style:
+        " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
+    });
+
+    const wrapThird = tagMaker("div", document.body, {
+      id: "wrap",
+      style:
+        " width:100vw; height:100vh; display:flex; justify-content:center; align-items:center;",
+    });
+
     //! 테스트
+    const titleText = tagMaker("h1", wrap, {
+      id: "titleText",
+      innerText: "POKE X DIGI",
+    });
     const colorPaper = tagMaker("div", wrap, { id: "colorPaper" });
+    const colorPaperSecond = tagMaker("div", wrapSecond, { id: "colorPaper" });
+    const colorPaperThird = tagMaker("div", wrapThird, { id: "colorPaper" });
     const container = tagMaker("div", wrap, { id: "container" });
 
     //! 버튼 만들기
@@ -70,5 +89,37 @@ sendCookie((data) => {
     btnback2.addEventListener("click", function () {
       location.href = "/src/views/html/createAccount.html";
     });
+    let currentSection = 1;
+
+    function handleMouseWheel(event) {
+      // 마우스 휠 이벤트 발생 시 실행될 함수
+      event.preventDefault(); // 기본 스크롤 동작 막기
+
+      const delta = event.wheelDelta || -event.detail;
+
+      if (delta > 0 && currentSection > 1) {
+        // 마우스 휠을 위로 굴릴 때 이전 섹션으로 스크롤
+        currentSection--;
+      } else if (delta < 0 && currentSection < 3) {
+        // 마우스 휠을 아래로 굴릴 때 다음 섹션으로 스크롤
+        currentSection++;
+      }
+
+      scrollToSection();
+    }
+
+    function scrollToSection() {
+      // 현재 섹션에 해당하는 영역으로 스크롤
+      if (currentSection === 1) {
+        wrap.scrollIntoView({ behavior: "smooth" });
+      } else if (currentSection === 2) {
+        wrapSecond.scrollIntoView({ behavior: "smooth" });
+      } else if (currentSection === 3) {
+        wrapThird.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    // 마우스 휠 이벤트 리스너 등록
+    window.addEventListener("wheel", handleMouseWheel, { passive: false });
   }
 });
