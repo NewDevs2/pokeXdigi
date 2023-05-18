ef8171a714b9323fa8f0d4e43a101ca0b6670aee
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 21:19:54 +0900

    이게 아닌가벼

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,6 +1,8 @@
 function test(clientInput) {
-    let checkForm = /^[0-1]{3}[- ]?[1-9]{4}[- ]?[1-9]{4}$/;
+    let checkForm = /^[0-1]{3}[- ]?[1-9]{1}[0-9]{3}[[- ]?[1-9]{1}[0-9]{3}$/;
     console.log(checkForm.test(clientInput));
     return checkForm.test(clientInput);
 }
 test('010-3333-2222')
+// 휴대폰 중간번호를 수정해야 함
+test('01010112222')
