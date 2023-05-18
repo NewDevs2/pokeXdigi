f51d9c2f82b5f2f6eb17c3c72ed4b9109733f3f8
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 21:30:59 +0900

    휴대폰 번호 정규식 작성 완료

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,8 +1,8 @@
 function test(clientInput) {
-    let checkForm = /^(010|011)[- ]?[1-9][0-9][- ]?[1-9][0-9]$/;
+    let checkForm = /^(010|011)[1-9][0-9]{7}$/;
     console.log(checkForm.test(clientInput));
     return checkForm.test(clientInput);
 }
-test('01033332222')
+test('0103333222')
 // 휴대폰 중간번호를 수정해야 함
-test('01010112222')
+test('01010111222')
