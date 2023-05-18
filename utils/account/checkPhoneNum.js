7d51c4888a32c85be9d034738b6687d2b0e09d8d
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 21:14:04 +0900

    휴대폰 검사 정규식 안되는 것 같음

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,4 +1,6 @@
 function test(clientInput) {
-    let checkForm = /^[0-1]{3}[- ]?[1-9]{3}[- ]?[1-9]{3}$/
+    let checkForm = /^[0-1]{3}[- ]?[1-9]{3}[- ]?[1-9]{3}$/;
+    console.log(checkForm.test(clientInput));
     return checkForm.test(clientInput);
 }
+test('010-3333-2222')
