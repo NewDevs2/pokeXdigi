1565145c644c78e9938213df58f76304333b4653
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 21:31:24 +0900

    휴대폰 번호 정규식 모듈 export

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,8 +1,8 @@
-function test(clientInput) {
+export default test(clientInput) {
     let checkForm = /^(010|011)[1-9][0-9]{7}$/;
-    console.log(checkForm.test(clientInput));
+    // console.log(checkForm.test(clientInput));
     return checkForm.test(clientInput);
 }
-test('0103333222')
-// 휴대폰 중간번호를 수정해야 함
-test('01010111222')
+// test('0103333222')
+// // 휴대폰 중간번호를 수정해야 함
+// test('01010111222')
