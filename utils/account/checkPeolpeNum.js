3490aed109526886df85fe950e269f6564d6bea2
choboruru <wkdfnqls6806@gmail.com>
2023-05-18 21:10:14 +0900

    모듈 외에 주석처리

--- utils/account/checkPeolpeNum.js
+++ utils/account/checkPeolpeNum.js
@@ -6,10 +6,10 @@ export default function(clientInput) {
     // [0-9]$ 0~9 숫자 {6자리의 숫자} -> 고유 번호
     // $ 문자의 끝
     let checkForm = /^[0-9]{6}[- ]?[1-4][0-9]{5}$/;
-    console.log(checkForm.test(clientInput))
+    // console.log(checkForm.test(clientInput))
     // return checkForm.test(clientInput);
 }
 // true
-test('123456-123456');
+// test('123456-123456');
 // // false
 // test('123457523456');
