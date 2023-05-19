fec935323c2beaf7fa1d99948460d92767bd25a0
jangrubin2 <wkdfnqls2465@gmail.com>
2023-05-19 12:36:22 +0900

    휴대폰 번호 확인 모듈 export

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,8 +1,9 @@
-function sss(clientInput) {
+export default function(clientInput) {
     let checkForm = /^(010|011|016|017|018|019)[1-9][0-9]{3}[1-9][0-9]{3}$/;
-    console.log(checkForm.test(clientInput));
+    // console.log(checkForm.test(clientInput));
     // return checkForm.test(clientInput);
+    return checkForm.test(clientInput)
 }
-// test('0103333222')
+// checkPhoneNum('01033332222')
 // // 휴대폰 중간번호를 수정해야 함
-sss('01020111222')
+// sss('01020111222')
