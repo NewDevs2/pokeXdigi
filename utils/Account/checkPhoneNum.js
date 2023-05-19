c2aea38d5072f08cc8d72d0836e58100ff3a9c69
KwakYounHo <kwakyh951@gmail.com>
2023-05-19 13:09:06 +0900

    미사용파일삭제

--- utils/Account/checkPhoneNum.js
+++
@@ -1,8 +0,0 @@
-export default function test(clientInput) {
-  let checkForm = /^(010|011)[1-9][0-9]{7}$/;
-  // console.log(checkForm.test(clientInput));
-  return checkForm.test(clientInput);
-}
-// test('0103333222')
-// // 휴대폰 중간번호를 수정해야 함
-// test('01010111222')
--- utils/Account/emailValidation.js
+++
@@ -1,8 +0,0 @@
-export default function (text) {
-  const regExp = /^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
-  if (regExp.test(text)) {
-    return true;
-  } else {
-    return false;
-  }
-}
