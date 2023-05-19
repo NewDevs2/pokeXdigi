35daa5181137fe43724c26eb996820ee30b33d7e
jangrubin2 <wkdfnqls2465@gmail.com>
2023-05-19 12:37:50 +0900

    잡다한 주석 제거

--- utils/account/checkPhoneNum.js
+++ utils/account/checkPhoneNum.js
@@ -1,9 +1,4 @@
 export default function(clientInput) {
     let checkForm = /^(010|011|016|017|018|019)[1-9][0-9]{3}[1-9][0-9]{3}$/;
-    // console.log(checkForm.test(clientInput));
-    // return checkForm.test(clientInput);
     return checkForm.test(clientInput)
 }
-// checkPhoneNum('01033332222')
-// // 휴대폰 중간번호를 수정해야 함
-// sss('01020111222')
