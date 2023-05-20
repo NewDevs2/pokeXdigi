f0e061a165e865432754a511897ca4dfca9e3d9d
HaekyungSeong <haekyungs95@gmail.com>
2023-05-20 15:36:02 +0900

    파일명 리네임

--- utils/JWT/jwtSignModule.js
+++
@@ -1,7 +0,0 @@
-export const makeTokenSign = (jwt, payload, secretKey, option) => {
-  const token =[
-    accessToken = jwt.sign(payload.acess, secretKey, option.access),
-    refreshToken = jwt.sign(payload.refresh, secretKey, option.refresh)
-  ]
-  return token
-}
