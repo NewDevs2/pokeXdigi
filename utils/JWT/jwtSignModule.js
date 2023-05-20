ec904f40fdb7b9319f04bae20bb3ba8b20a22484
HaekyungSeong <haekyungs95@gmail.com>
2023-05-20 13:40:10 +0900

    sign 모듈화 윤호님이랑 얘기하면서 수정한 부분

--- utils/JWT/jwtSignModule.js
+++ utils/JWT/jwtSignModule.js
@@ -1,6 +1,7 @@
-exports = {
-  secretKey:'Hello',
-  Option:{
-    exp:'3s'
-  }
+export const makeTokenSign = (jwt, payload, secretKey, option) => {
+  const token =[
+    accessToken = jwt.sign(payload.acess, secretKey, option.access),
+    refreshToken = jwt.sign(payload.refresh, secretKey, option.refresh)
+  ]
+  return token
 }
