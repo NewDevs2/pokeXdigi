a97e98b8b5844b837cf32fc5ae53b24231bb4d0a
HaekyungSeong <haekyungs95@gmail.com>
2023-05-21 18:45:36 +0900

    소캣으로 접속 유저별 캐릭터 생성 구현

--- src/views/js/chat.js
+++ src/views/js/chat.js
@@ -1,4 +1,5 @@
 import { sendCookie } from "../../../utils/Cookie/cookieManager.js";
+import tagMaker from "../../models/tag/tagMaker.js";
 
 //! = 바뀌어야 할 내용 시작점
 //? = 바뀌어야 할 내용 끝 점
@@ -17,12 +18,84 @@ window.onload = (() => {
     }
   });
   // 새로운 접속자가 생겼을 때 안내 문구
+
+  let character
+
   socket.on("enterUser", (data) => {
     const element = document.createElement("p");
     element.innerText = `[ ${data} ]님이 입장하셨습니다.`;
     chatBox.appendChild(element);
     chatBox.scrollTop = chatBox.scrollHeight;
+
+    // 입장한 유저와 온라인 유저들 캐릭터 생성
+    socket.on("enterUserCharacter", (data) => {
+      // console.log(data)
+
+      // 입장한 유저의 nickname을 아이디로 하는 캐릭터를 생성한다.
+      tagMaker("div", field, {
+        id: data[0],
+        className: "character",
+      });
+
+      // 캐릭터변수에 접속 유저 아이디의 캐릭터를 담는다.
+      character = document.getElementById(data[0])
+
+      // 반복문을 이용해서 온라인 유저만큼의 캐릭터 생성을 한다.
+      for (let i = 0; i < data[1].length; i++) {
+
+        // 온라인 유저의 아이디와 위치정보를 id와 style 적용을 해서 한다.
+        tagMaker("div", field, {
+          id: data[1][i].nickname,
+          className: "character",
+          style: `left:${data[1][i].position[0]}px; top:${data[1][i].position[1]}px;`
+        })
+      }
+    })
   });
+
+  // 새로 접속한 유저의 캐릭터 생성 이벤트
+  socket.on("newUserCharacter", (data) => {
+    // 해당 유저의 닉네임을 id 로 하는 캐릭터 생성을 해준다.
+    tagMaker("div", field, {
+      id: data,
+      className: "character",
+    });
+  })
+
+  // ! 캐릭터 이동 이벤트
+  let leftPosition = 0;
+  let topPosition = 0;
+  document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        leftPosition -= 50;
+        break;
+      case "ArrowRight":
+        leftPosition += 50;
+        break;
+      case "ArrowUp":
+        topPosition -= 50;
+        break;
+      case "ArrowDown":
+        topPosition += 50;
+        break;
+    }
+    character.style.left = leftPosition + "px";
+    character.style.top = topPosition + "px";
+
+    // 움직인 정보를 이벤트로 전달한다.
+    socket.emit("userPosition", [leftPosition, topPosition])
+  });
+
+  // 다른 유저가 움직인 경우의 이벤트
+  socket.on("moveUser", (data) => {
+    // 움직인 유저의 nickname 으로 해당 캐릭터를 변수에 담아준다.
+    const moveUser = document.getElementById(data.nickname)
+
+    // 움직인 데이터를 style 적용한다.
+    moveUser.style.left = data.position[0] + "px"
+    moveUser.style.top = data.position[1] + "px"
+  })
 
   // 채팅 메세지를 수신했을 때 내용
   socket.on("chat", (data) => {
@@ -47,13 +120,17 @@ window.onload = (() => {
     element.innerText = `[ ${data} ]님이 퇴장하셨습니다.`;
     chatBox.appendChild(element);
     chatBox.scrollTop = chatBox.scrollHeight;
+
+    // 퇴장한 유저의 캐릭터를 변수에 담아 display를 none으로 변경하여 필드에서 사라지게 한다.
+    const exitUser = document.getElementById(data)
+    exitUser.style.display = "none"
   })
 
   socket.on("error", data => {
     alert('너 팅겼어');
     location.href = "/src/views/html/index.html"
   })
-  
+
   // 폼 이벤트
   const form = document.getElementById("chattingForm");
   const chatText = form.children[0];
--- utils/Socket/socketServer.js
+++ utils/Socket/socketServer.js
@@ -10,6 +10,9 @@ export default function socketServer(server) {
 
   const io = new Server(server, { path: "/chat/" });
 
+  // 온라인 상태의 유저를 담을 배열
+  let onlineUser = []
+
   // 서버 연결
   io.on("connection", (socket) => {
     // 유저가 입장되어 입장된 유저 id 를 클라이언트로 부터 전달받았을 때
@@ -17,13 +20,37 @@ export default function socketServer(server) {
       // 해당 id 를 socket.nickname 에 담아준다.
       socket.nickname = data;
       console.log(socket.nickname);
+
       // 전체 유저에게 해당 유저 입장을 알려준다.
       io.emit("enterUser", socket.nickname);
+
+      // 접속한 유저의 캐릭터 생성용 이벤트
+      // 접속한 유저 및 온라인 상태의 유저들 정보를 같이 알려준다.
+      socket.emit("enterUserCharacter",[socket.nickname, onlineUser])
+
+      // 새로 입장한 유저 정보를 온라인 상태의 유저들에게 알려준다.
+      socket.broadcast.emit("newUserCharacter", socket.nickname)
+      
+      // 새로 들어온 유저정보를 온라인 유저배열에 추가한다.
+      onlineUser.push({nickname:socket.nickname,position:[0,0]})
     });
+
+    // 유저 위치정보에 대한 이벤트
+    socket.on("userPosition",(data)=>{
+      // find 메서드를 이용해서 움직인 유저 정보를 online 유저에서 찾는다.
+      let moveUser = onlineUser.find(user=>user.nickname===socket.nickname)
+
+      // 해당 유저의 위치정보 변경
+      moveUser.position = data
+
+      // 다른 유저들에게도 해당 정보를 전달(유저닉네임과 위치정보)
+      socket.broadcast.emit("moveUser",moveUser)
+    })
+
     // 클라이언트 측으로 부터 채팅을 전달받는다.
     socket.on("chat", (data) => {
       if (socket.nickname !== undefined) {
-        // console.log(socket.nickname);
+        console.log(socket.nickname);
         sign_master.query(
           "insert into chatting_log(ID, CHATTING_LOG) values(?,?)",
           [socket.nickname, `${data}`]
@@ -37,10 +64,15 @@ export default function socketServer(server) {
         socket.emit("error", { status: "nicknameError" });
       }
     });
+
     // 채팅이 끊겼을 때
     socket.on("disconnect", () => {
       // 모든 유저에게 서버에서 떠난 유저의 닉네임을 알려준다.
       io.emit("exitUser", socket.nickname);
+
+      // filter 메서드를 이용해서 온라인유저정보에서 퇴장한 유저정보를 제거해준다.
+      onlineUser = onlineUser.filter(user=>user.nickname!==socket.nickname)
+
     });
   });
 }
