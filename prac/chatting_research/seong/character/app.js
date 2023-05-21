efeaaa90eca1de23ed95546d490c8a6ec656d410
HaekyungSeong <haekyungs95@gmail.com>
2023-05-21 14:43:04 +0900

    나중에 입장하더라도 기존 유저까지 나타나도록은 해결. 근데 움직이질 않음.

--- prac/chatting_research/seong/character/app.js
+++ prac/chatting_research/seong/character/app.js
@@ -36,24 +36,45 @@ const server = http.createServer((req,res)=>{
 
 const io = new Server(server)
 
+const users = []
+
 io.on('connection', (socket)=>{
-  console.log('connected')
-  console.log(socket.id)
+  console.log(socket.id+'connected')
 
-  // 유저 접속 시, 본인에게 유저 아이디 전송
-  socket.emit('connected',socket.id)
-
-  // 유저 접속 시, 본인 제외 다른 사람들에게 유저 아이디 전송
+  // 유저 접속 시, 본인에게 유저 아이디랑 사전 접속된 아이디 전송
+  socket.emit('connected',[socket.id, users])
+  console.log(users)
+  
+  // 유저 접속 시, 본인 제외 기존 접속 유저들에게 유저 아이디 전송
   socket.broadcast.emit('anotherUserConnected',socket.id)
-
+  
+  users.push({id:socket.id,position:[0,0]})
   // ! io로 안하고 나눈 이유 : 생성될 div 의 이름을 구분 주려고
 
   // 움직일 경우 이동정보 데이터 수신(배열로)
   socket.on('moving',(data)=>{
-    console.log(data)
+    console.log(data+'moving')
+
+    let moveUser = users.find(user=>user.id===socket.id)
+
+    moveUser.position = data
+
+    console.log(users+'moving')
 
     // 본인 외의 다른 유저들에게 움직인 사람의 유저아이디와 움직인 정보 전송
-    socket.broadcast.emit('movingAnotherUser',[socket.id,data])
+    socket.broadcast.emit('movingAnotherUser', moveUser)
   })
+
+  socket.on('disconnect',()=>{
+    console.log(socket.id)
+    
+    io.emit('disconnected',socket.id)
+
+    users = users.filter(user=>user.id===socket.id)
+    console.log(users+'remove')
+
+
+  })
+
 })
 
--- prac/chatting_research/seong/character/socketcharactor.js
+++ prac/chatting_research/seong/character/socketcharactor.js
@@ -1,8 +1,7 @@
 const socket = io()
-
-const createDoc=(tag,attri,parent)=>{
+const createDoc = (tag, attri, parent) => {
   const element = document.createElement(tag)
-  Object.assign(element,attri)
+  Object.assign(element, attri)
   parent.appendChild(element)
   return element
 }
@@ -11,13 +10,23 @@ let user
 
 // 내가 접속했을 때, 내 소켓아이디를 아이디로 가지는 div 생성.
 // 나랑 타인을 구분하려고 나는 div 색상을 빨강으로 함
-socket.on('connected',(data)=>{
-  createDoc('div',{
-    id:data,
-    style : 'width: 300px; height: 300px; background-color: red; position: absolute;font-size:20px; text-align:center;'
-  },document.body)
-  user=document.getElementById(data)
+socket.on('connected', (data) => {
+  // 지금 입장한 나
+  createDoc('div', {
+    id: data[0],
+    style: 'width: 300px; height: 300px; background-color: red; position: absolute;font-size:20px; text-align:center;'
+  }, document.body)
+  user = document.getElementById(data)
   // })
+
+  // 사전 입장한 사람들
+  for (let i = 0; i < data[1].length; i++) {
+    createDoc('div', {
+      id: data[1][i].id,
+      style: `width: 300px; height: 300px; background-color: aqua; position: absolute;font-size:20px; text-align:center; left:${data[1][i].position[0]}; top:${data[1][i].position[1]};`
+    }, document.body)
+  }
+
 });
 
 
@@ -54,22 +63,27 @@ document.addEventListener("keydown", (event) => {
   user.style.top = topPosition + "px";
 
   // 내가 움직이면 그 움직인 정보를 socket 이벤트로 전달
-  socket.emit('moving',[leftPosition,topPosition])
+  socket.emit('moving', [leftPosition, topPosition])
 
-// 나 외의 타인이 움직인 경우
-socket.on('movingAnotherUser',(data)=>{
-  console.log(data)
+  // 나 외의 타인이 움직인 경우
+  socket.on('movingAnotherUser', (data) => {
+    console.log(data)
 
-  // 해당 유저의 소켓아이디의 div 을 변수에 담는다.
-  const anotherUser = document.getElementById(data[0])
+    // 해당 유저의 소켓아이디의 div 을 변수에 담는다.
+    const anotherUser = document.getElementById(data.id)
 
-  // 그 변수의 스타일에 이동정보 입력
-  anotherUser.style.left=data[1][0]+"px"
-  anotherUser.style.top=data[1][1]+"px"
+    // 그 변수의 스타일에 이동정보 입력
+    anotherUser.style.left = data.position[0] + "px"
+    anotherUser.style.top = data.position[1] + "px"
 
-  // console.log(data)
-  // const anotherUser = document.createElement('div')
-  // document.body.appendChild(anotherUser)
+    // console.log(data)
+    // const anotherUser = document.createElement('div')
+    // document.body.appendChild(anotherUser)
+  })
+
 })
 
+socket.on('disconnected',(data)=>{
+  const disconnectedUser = document.getElementById(data)
+  disconnectedUser.style.display='none'
 })
