e56839a0745eca755ceb8df6c390670228b5cd00
KwakYounHo <kwakyh951@gmail.com>
2023-05-25 18:59:04 +0900

    제작 중

---
+++ utils/JWT/jwtToken.js
@@ -0,0 +1,4 @@
+import { secretKey } from "./secretKey.js";
+import { makeToken } from "./makeToken.js";
+
+function firstLogin(uid, )
--- utils/JWT/makeToken.js
+++ utils/JWT/makeToken.js
@@ -1,12 +1,10 @@
-export const makeToken = (jwt, payload=[{},{}], secretKey, option=[{},{}]) => {
+import jwt from 'jsonwebtoken'
+export const makeToken = (payload={}, secretKey, option={}) => {
   // payload, option 이 없을 수도 있어서 매개변수 디폴트값 설정
 
   // 2개의 토큰을 배열로 만듦.
   // 0번째 인덱스 = accessToken / 1번째 인덱스는 refreshToken
-  const token =[
-    jwt.sign(payload[0], secretKey, option[0]),
-    jwt.sign(payload[1], secretKey, option[1])
-  ]
+  const token = jwt.sign(payload, secretKey, option)
   return token
 }
 
